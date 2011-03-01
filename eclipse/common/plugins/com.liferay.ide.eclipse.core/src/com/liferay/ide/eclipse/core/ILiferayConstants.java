dc11b842d6dce428a5dd1b56b6853a42fbf7dd21
Gregory Amerson <gregory.amerson@liferay.com>
2011-03-01 20:29:35 +0000

    Moved constants IPortalConstants to Liferay and Server
    git-svn-id: svn://svn.liferay.com/repos/public/tools/trunk/ide@74231 05bdf26c-840f-0410-9ced-eb539d925f36

--- eclipse/common/plugins/com.liferay.ide.eclipse.core/src/com/liferay/ide/eclipse/core/ILiferayConstants.java
+++ eclipse/common/plugins/com.liferay.ide.eclipse.core/src/com/liferay/ide/eclipse/core/ILiferayConstants.java
@@ -1,8 +1,19 @@
 package com.liferay.ide.eclipse.core;
 
+import org.osgi.framework.Version;
+
 
 public interface ILiferayConstants {
 
 	String LIFERAY_PLUGIN_PACKAGE_PROPERTIES_FILE = "liferay-plugin-package.properties";
+	String LIFERAY_DISPLAY_XML_FILE = "liferay-display.xml";
+	String LIFERAY_HOOK_XML_FILE = "liferay-hook.xml";
+	String LIFERAY_LAYOUTTPL_XML_FILE = "liferay-layout-templates.xml";
+	String LIFERAY_LOOK_AND_FEEL_XML_FILE = "liferay-look-and-feel.xml";
+	String LIFERAY_PORTLET_XML_FILE = "liferay-portlet.xml";
+	String LIFERAY_SERVICE_BUILDER_XML_FILE = "service.xml";
+	String PORTLET_XML_FILE = "portlet.xml";
+	String WEB_XML_FILE = "web.xml";
+	Version LEAST_SUPPORTED_VERSION = new Version(6, 0, 2);
 
 }
---
+++ eclipse/server/plugins/com.liferay.ide.eclipse.server.core/src/com/liferay/ide/eclipse/server/core/IServerConstants.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2000-2011 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.eclipse.server.core;
+
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+/**
+ * @author Greg Amerson
+ */
+public interface IServerConstants {
+
+	public static final IEclipsePreferences defaultPrefs = new DefaultScope().getNode(LiferayServerCorePlugin.PLUGIN_ID);
+
+	String INSTALLABLE_UPDATE_URL = defaultPrefs.get("installable.update.url", "");
+
+}
