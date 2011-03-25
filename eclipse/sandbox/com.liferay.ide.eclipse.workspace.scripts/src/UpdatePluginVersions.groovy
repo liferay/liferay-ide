40409b28b54675eb507585f3bb09881d86761779
Gregory Amerson <gregory.amerson@liferay.com>
2011-03-25 16:20:06 +0000

    Preparing for Studio release, updating IDE to 1.2.3 and Studio to 1.2.0
    git-svn-id: svn://svn.liferay.com/repos/public/tools/trunk/ide@76176 05bdf26c-840f-0410-9ced-eb539d925f36

--- eclipse/sandbox/com.liferay.ide.eclipse.workspace.scripts/src/UpdatePluginVersions.groovy
+++ eclipse/sandbox/com.liferay.ide.eclipse.workspace.scripts/src/UpdatePluginVersions.groovy
@@ -1,10 +1,11 @@
-File workspaceDir = new File("..")
-def oldVersion = "1.0.0.qualifier"
-def newVersion = "1.0.1.qualifier"
 
-workspaceDir.eachDir { 
+File workspaceDir = new File("..")
+def oldVersion = "1.2.0.qualifier"
+def newVersion = "1.2.3.qualifier"
+
+workspaceDir.eachDir {
 	File manifest = new File(it, "META-INF/MANIFEST.MF")
-	
+
 	if (manifest.exists()) {
 		String fileText = manifest.text;
 		if (fileText.contains("Bundle-Vendor: Liferay")) {
