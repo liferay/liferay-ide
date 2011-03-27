635dc605caa9e540ce47e3f9e7f461bec70f460b
Gregory Amerson <gregory.amerson@liferay.com>
2011-03-27 21:43:01 +0000

    IDE-329 fix
    git-svn-id: svn://svn.liferay.com/repos/public/tools/trunk/ide@76252 05bdf26c-840f-0410-9ced-eb539d925f36

--- eclipse/server/plugins/com.liferay.ide.eclipse.server.ui/src/com/liferay/ide/eclipse/server/ui/action/OpenPortalAction.java
+++ eclipse/server/plugins/com.liferay.ide.eclipse.server.ui/src/com/liferay/ide/eclipse/server/ui/action/OpenPortalAction.java
@@ -26,6 +26,7 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.wst.server.core.IServer;
@@ -62,19 +63,27 @@ public class OpenPortalAction extends AbstractServerRunningAction {
 		return IServer.STATE_STARTED;
 	}
 
-	protected void openBrowser(URL url, String browserTitle) {
-		try {
-			IWorkbenchBrowserSupport browserSupport = ServerUIPlugin.getInstance().getWorkbench().getBrowserSupport();
+	protected void openBrowser(final URL url, final String browserTitle) {
+		Display.getDefault().asyncExec(new Runnable() {
 
-			IWebBrowser browser =
-				browserSupport.createBrowser(IWorkbenchBrowserSupport.LOCATION_BAR |
-					IWorkbenchBrowserSupport.NAVIGATION_BAR, null, browserTitle, null);
+			public void run() {
+				try {
+					IWorkbenchBrowserSupport browserSupport =
+						ServerUIPlugin.getInstance().getWorkbench().getBrowserSupport();
 
-			browser.openURL(url);
-		}
-		catch (Exception e) {
-			LiferayServerUIPlugin.logError(e);
-		}	}
+					IWebBrowser browser =
+						browserSupport.createBrowser(IWorkbenchBrowserSupport.LOCATION_BAR |
+							IWorkbenchBrowserSupport.NAVIGATION_BAR, null, browserTitle, null);
+
+					browser.openURL(url);
+				}
+				catch (Exception e) {
+					LiferayServerUIPlugin.logError(e);
+				}
+			}
+		});
+
+	}
 
 	protected void openPortal(ILiferayServer portalServer) {
 		URL portalHome = portalServer.getPortalHomeUrl();
