9d042d350d93254bb713dc5f731678528da96a5a
Kamesh Sampath <kamesh.sampath@hotmail.com>
2011-08-17 08:44:37 +0530

    OPEN - issue IDE-301: Add a specialized editor for portlet.xml http://issues.liferay.com/browse/IDE-301
    Adding the missing file to the repository

---
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.core/src/com/liferay/ide/eclipse/portlet/core/util/PortletAppModelConstants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+package com.liferay.ide.eclipse.portlet.core.util;
+
+/**
+ * @author kamesh
+ */
+public interface PortletAppModelConstants {
+
+	public static final String DEFAULT_QNAME_PREFIX = "qns";
+	public static final String LOCAL_PART_DEFAULT_VALUE = "LOCAL_PART";
+	public static final String NAMESPACE_URI_DEFAULT_VALUE = "NAMESPACE_URI";
+	public static final String DEFAULT_DERIVED_QNAME_VALUE = "{NAMESPACE_URI}LOCAL_PART";
+}
