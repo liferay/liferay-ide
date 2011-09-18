361302a592e20887b295b8c8efce3a9621eefced
Kamesh Sampath <kamesh.sampath@hotmail.com>
2011-09-18 22:05:41 +0530

    OPEN - issue IDE-405: portlet.xml editor reorganization http://issues.liferay.com/browse/IDE-405 Removed 1.0 version from the version list

--- eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.core/src/com/liferay/ide/eclipse/portlet/core/model/PortletAppVersion.java
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.core/src/com/liferay/ide/eclipse/portlet/core/model/PortletAppVersion.java
@@ -25,10 +25,6 @@ import org.eclipse.sapphire.modeling.annotations.Label;
  */
 @Label( standard = "Portlet Version" )
 public enum PortletAppVersion {
-	@Label( standard = "1.0" )
-	@EnumSerialization( primary = "1.0" )
-	V_1_0,
-
 	@Label( standard = "2.0" )
 	@EnumSerialization( primary = "2.0" )
 	V_2_0,
