b59a02ac7e623250a4fedc0214e0610a5428cf34
Kamesh Sampath <kamesh.sampath@hotmail.com>
2011-09-21 22:58:20 +0530

    IN PROGRESS - issue IDE-405: portlet.xml editor reorganization http://issues.liferay.com/browse/IDE-405 Fixed the /public-render-parameter/name and /event-definition/name validation

--- eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.core/src/com/liferay/ide/eclipse/portlet/core/model/internal/QNameLocalPartValueBinding.java
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.core/src/com/liferay/ide/eclipse/portlet/core/model/internal/QNameLocalPartValueBinding.java
@@ -114,12 +114,26 @@ extends XmlValueBindingImpl
 			qNameElement = parent.getChildElement( params[0], true );
 		}
 
-		// System.out.println( "VALUE ___________________ " + val );
-
 		// System.out.println( "TextNodeValueBinding.write() - Parent " + xml( true ).getParent() );
-		if ( qNameElement != null && val != null ) {
-			val = value.trim();
-			qNameElement.setText( PortletAppModelConstants.DEFAULT_QNAME_PREFIX + ":" + val );
+		if ( qNameElement != null ) {
+			val = val != null ? value.trim() : "";
+			if ( params.length == 2 && "localpart".equals( params[1] ) ) { // update only local part
+				// System.out.println( "VALUE ___________________ " + val );
+				String existingText = qNameElement.getText();
+				if ( existingText != null && existingText.indexOf( ":" ) != -1 ) {
+					String updatedLocalPart = existingText.substring( 0, ( existingText.indexOf( ":" ) + 1 ) );
+					updatedLocalPart = updatedLocalPart + val;
+					// System.out.println( "Updated value ___________________ " + updatedLocalPart );
+					qNameElement.setText( updatedLocalPart );
+				}
+				else {
+					qNameElement.setText( PortletAppModelConstants.DEFAULT_QNAME_PREFIX + ":" + val );
+				}
+
+			}
+			else {
+				qNameElement.setText( PortletAppModelConstants.DEFAULT_QNAME_PREFIX + ":" + val );
+			}
 		}
 
 	}
