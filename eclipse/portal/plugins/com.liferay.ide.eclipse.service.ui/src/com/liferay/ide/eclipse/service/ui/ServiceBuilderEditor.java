b261235d4f9f3d56157e561416aa03ba1418e5e6
Gregory Amerson <gregory.amerson@liferay.com>
2011-11-28 23:59:10 -0600

    Readded graphiti dependency for service builder editor

--- eclipse/portal/plugins/com.liferay.ide.eclipse.service.ui/src/com/liferay/ide/eclipse/service/ui/ServiceBuilderEditor.java
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.service.ui/src/com/liferay/ide/eclipse/service/ui/ServiceBuilderEditor.java
@@ -20,6 +20,13 @@ package com.liferay.ide.eclipse.service.ui;
 
 import com.liferay.ide.eclipse.service.core.model.IServiceBuilder;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.sapphire.ui.swt.graphiti.editor.SapphireDiagramEditor;
+import org.eclipse.sapphire.ui.swt.graphiti.editor.SapphireDiagramEditorFactory;
+import org.eclipse.sapphire.ui.swt.graphiti.editor.SapphireDiagramEditorInput;
 import org.eclipse.sapphire.ui.swt.xml.editor.SapphireEditorForXml;
 import org.eclipse.ui.PartInitException;
 
@@ -29,7 +36,7 @@ import org.eclipse.ui.PartInitException;
  */
 public class ServiceBuilderEditor extends SapphireEditorForXml {
 
-	// private SapphireDiagramEditor pageDiagram;
+	private SapphireDiagramEditor pageDiagram;
 
 	public ServiceBuilderEditor() {
 		super(ServiceUI.PLUGIN_ID);
@@ -39,34 +46,39 @@ public class ServiceBuilderEditor extends SapphireEditorForXml {
 			"/com/liferay/ide/eclipse/service/ui/ServiceBuilder.sdef/serviceBuilderPage" );
 	}
 
-	// @Override
-	// public void doSave(final IProgressMonitor monitor) {
-	// super.doSave(monitor);
-	//
-	// this.pageDiagram.doSave(monitor);
-	// }
+	@Override
+	public void doSave( final IProgressMonitor monitor )
+	{
+		super.doSave( monitor );
 
-	// @Override
-	// protected void createDiagramPages() throws PartInitException {
-	// IPath path =
-	// new Path( ServiceUI.PLUGIN_ID + "/com/liferay/ide/eclipse/service/ui/ServiceBuilder.sdef/diagramPage" );
-	// this.pageDiagram = new SapphireDiagramEditor(this.getModelElement(), path);
-	// SapphireDiagramEditorInput diagramEditorInput = null;
-	//
-	// try {
-	// diagramEditorInput =
-	// SapphireDiagramEditorFactory.createEditorInput(this.getModelElement().adapt(IFile.class));
-	// }
-	// catch (Exception e) {
-	// ServiceUI.logError(e);
-	// }
-	//
-	// if (diagramEditorInput != null) {
-	// addPage(0, this.pageDiagram, diagramEditorInput);
-	// setPageText(0, "Diagram");
-	// setPageId(this.pages.get(0), "Diagram", this.pageDiagram.getPart());
-	// }
-	// }
+		this.pageDiagram.doSave( monitor );
+	}
+
+	@Override
+	protected void createDiagramPages() throws PartInitException
+	{
+		IPath path =
+			new Path( ServiceUI.PLUGIN_ID + "/com/liferay/ide/eclipse/service/ui/ServiceBuilder.sdef/diagramPage" );
+		this.pageDiagram = new SapphireDiagramEditor( this.getModelElement(), path );
+		SapphireDiagramEditorInput diagramEditorInput = null;
+
+		try
+		{
+			diagramEditorInput =
+				SapphireDiagramEditorFactory.createEditorInput( this.getModelElement().adapt( IFile.class ) );
+		}
+		catch ( Exception e )
+		{
+			ServiceUI.logError( e );
+		}
+
+		if ( diagramEditorInput != null )
+		{
+			addPage( 0, this.pageDiagram, diagramEditorInput );
+			setPageText( 0, "Diagram" );
+			setPageId( this.pages.get( 0 ), "Diagram", this.pageDiagram.getPart() );
+		}
+	}
 
 	@Override
 	protected void createFormPages() throws PartInitException {
