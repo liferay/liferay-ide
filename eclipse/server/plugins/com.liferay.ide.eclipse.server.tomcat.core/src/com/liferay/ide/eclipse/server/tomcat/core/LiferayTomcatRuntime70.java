318b9a0e9375fcf0ece8c95cc01856b6b2e3a777
Gregory Amerson <gregory.amerson@liferay.com>
2012-02-15 17:29:56 +0800

    Added 60,70 subclasses to LiferayTomcatRuntime

---
+++ eclipse/server/plugins/com.liferay.ide.eclipse.server.tomcat.core/src/com/liferay/ide/eclipse/server/tomcat/core/LiferayTomcatRuntime70.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *     Greg Amerson <gregory.amerson@liferay.com>
+ *******************************************************************************/
+
+package com.liferay.ide.eclipse.server.tomcat.core;
+
+import org.osgi.framework.Version;
+
+public class LiferayTomcatRuntime70 extends LiferayTomcatRuntime
+{
+
+	protected Version getLeastSupportedVersion() {
+		return new Version( 6, 1, 0 );
+	}
+
+
+}
