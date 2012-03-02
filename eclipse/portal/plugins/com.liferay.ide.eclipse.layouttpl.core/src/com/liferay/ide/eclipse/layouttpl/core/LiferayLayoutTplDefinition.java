45162c4bbf3073d701629027814c4f4a45cc0182
Gregory Amerson <gregory.amerson@liferay.com>
2012-03-02 14:18:56 +0800

    IDE-562

--- eclipse/portal/plugins/com.liferay.ide.eclipse.layouttpl.core/src/com/liferay/ide/eclipse/layouttpl/core/LiferayLayoutTplDefinition.java
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.layouttpl.core/src/com/liferay/ide/eclipse/layouttpl/core/LiferayLayoutTplDefinition.java
@@ -15,16 +15,27 @@
 package com.liferay.ide.eclipse.layouttpl.core;
 
 import com.liferay.ide.eclipse.project.core.AbstractProjectDefinition;
+import com.liferay.ide.eclipse.project.core.facet.IPluginFacetConstants;
+import com.liferay.ide.eclipse.project.core.facet.IPluginProjectDataModelProperties;
 import com.liferay.ide.eclipse.project.core.util.ProjectUtil;
 
+import org.eclipse.jst.j2ee.project.facet.IJ2EEFacetConstants;
+import org.eclipse.jst.j2ee.web.project.facet.IWebFacetInstallDataModelProperties;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
 import org.eclipse.wst.common.project.facet.core.IFacetedProjectWorkingCopy;
 
 
-public class LiferayLayoutTplDefinition extends AbstractProjectDefinition {
+public class LiferayLayoutTplDefinition extends AbstractProjectDefinition implements IPluginProjectDataModelProperties
+{
 
 	public void setupNewProject(IDataModel dataModel, IFacetedProjectWorkingCopy facetedProject) {
 		ProjectUtil.setGenerateDD(dataModel, false);
+
+		FacetDataModelMap map = (FacetDataModelMap) dataModel.getProperty( FACET_DM_MAP );
+		IDataModel webFacetModel = map.getFacetDataModel( IJ2EEFacetConstants.DYNAMIC_WEB_FACET.getId() );
+
+		webFacetModel.setStringProperty(
+			IWebFacetInstallDataModelProperties.CONFIG_FOLDER, IPluginFacetConstants.PORTLET_PLUGIN_SDK_CONFIG_FOLDER );
 	}
 
 }
