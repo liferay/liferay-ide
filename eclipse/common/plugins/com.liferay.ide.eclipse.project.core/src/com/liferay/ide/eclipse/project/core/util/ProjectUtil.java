72d7fe16cb4424f979c76f62817dc68adbe1eb96
Gregory Amerson <gregory.amerson@liferay.com>
2012-05-16 10:42:46 +0800

    IDE-612

--- eclipse/common/plugins/com.liferay.ide.eclipse.project.core/src/com/liferay/ide/eclipse/project/core/util/ProjectUtil.java
+++ eclipse/common/plugins/com.liferay.ide.eclipse.project.core/src/com/liferay/ide/eclipse/project/core/util/ProjectUtil.java
@@ -244,9 +244,8 @@ public class ProjectUtil
 	public static void createDefaultWebXml( File webxmlFile )
 	{
 		final String webXmlContents =
-			"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE web-app PUBLIC \"-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN\" \"http://java.sun.com/dtd/web-app_2_3.dtd\">\n<web-app>\n</web-app>";
-
-		//"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<web-app id=\"WebApp_ID\" version=\"2.4\" xmlns=\"http://java.sun.com/xml/ns/javaee\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/j2ee/web-app_2_4.xsd\">\n</web-app>"; //$NON-NLS-1$
+			//"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE web-app PUBLIC \"-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN\" \"http://java.sun.com/dtd/web-app_2_3.dtd\">\n<web-app>\n</web-app>";
+		      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<web-app id=\"WebApp_ID\" version=\"2.5\" xmlns=\"http://java.sun.com/xml/ns/javaee\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd\">\n</web-app>"; //$NON-NLS-1$
 
 		try
 		{
