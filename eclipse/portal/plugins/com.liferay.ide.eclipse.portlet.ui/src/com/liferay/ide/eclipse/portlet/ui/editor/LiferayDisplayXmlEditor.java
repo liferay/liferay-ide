abafe4311e09a106cda3ef6bbb99b98188626d96
Gregory Amerson <gregory.amerson@liferay.com>
2012-06-11 17:55:29 +0800

    Updated Liferay display xml editor to use better subclass

--- eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.ui/src/com/liferay/ide/eclipse/portlet/ui/editor/LiferayDisplayXmlEditor.java
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.ui/src/com/liferay/ide/eclipse/portlet/ui/editor/LiferayDisplayXmlEditor.java
@@ -20,71 +20,24 @@ package com.liferay.ide.eclipse.portlet.ui.editor;
 
 import com.liferay.ide.eclipse.portlet.core.display.model.IDisplay;
 
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.sapphire.modeling.IModelElement;
-import org.eclipse.sapphire.modeling.xml.RootXmlResource;
-import org.eclipse.sapphire.ui.SapphireEditor;
-import org.eclipse.sapphire.ui.form.editors.masterdetails.MasterDetailsEditorPage;
-import org.eclipse.sapphire.ui.swt.xml.editor.XmlEditorResourceStore;
-import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.wst.sse.ui.StructuredTextEditor;
+import org.eclipse.sapphire.ui.swt.xml.editor.SapphireEditorForXml;
 
 /**
  * @author <a href="mailto:kamesh.sampath@accenture.com">Kamesh Sampath</a>
+ * @author Gregory Amerson
  */
-public class LiferayDisplayXmlEditor extends SapphireEditor {
+public class LiferayDisplayXmlEditor extends SapphireEditorForXml
+{
+    public static final String ID = "com.liferay.ide.eclipse.portlet.ui.editor.LiferayDisplayXmlEditor";
 
-	public static final String ID = "com.liferay.ide.eclipse.portlet.ui.editor.LiferayDisplayXmlEditor";
+    private static final String EDITOR_DEFINITION_PATH =
+        "com.liferay.ide.eclipse.portlet.ui/com/liferay/ide/eclipse/portlet/ui/editor/liferay-display.sdef/DetailsPage";
 
-	private static final String EDITOR_DEFINITION_PATH =
-		"com.liferay.ide.eclipse.portlet.ui/com/liferay/ide/eclipse/portlet/ui/editor/liferay-display.sdef/DetailsPage";
-
-	private StructuredTextEditor sourceEditor;
-	private MasterDetailsEditorPage formEditor;
-	private IModelElement model;
-
-	/**
-	 * 
-	 */
-	public LiferayDisplayXmlEditor() {
-		super( ID );
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.sapphire.ui.SapphireEditor#createModel()
-	 */
-	@Override
-	protected IModelElement createModel() {
-		// TODO Adapt the model based on the version of the DTD
-		RootXmlResource rootXmlResource = new RootXmlResource( new XmlEditorResourceStore( this, sourceEditor ) );
-		model = IDisplay.TYPE.instantiate( rootXmlResource );
-		return model;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.sapphire.ui.SapphireEditor#createSourcePages()
-	 */
-	@Override
-	protected void createSourcePages() throws PartInitException {
-		sourceEditor = new StructuredTextEditor();
-		sourceEditor.setEditorPart( this );
-		final IFileEditorInput fileEditorInput = (IFileEditorInput) getEditorInput();
-		int pgIndex = addPage( sourceEditor, fileEditorInput );
-		setPageText( pgIndex, "Source" );
-
-	}
-
-	@Override
-	protected void createFormPages() throws PartInitException {
-		IPath pageDefinitionLocation = new Path( EDITOR_DEFINITION_PATH );
-		formEditor = new MasterDetailsEditorPage( this, model, pageDefinitionLocation );
-		addEditorPage( 0, formEditor );
-	}
+    public LiferayDisplayXmlEditor()
+    {
+        super( ID );
+        setEditorDefinitionPath( EDITOR_DEFINITION_PATH );
+        setRootModelElementType( IDisplay.TYPE );
+    }
 
 }
-
