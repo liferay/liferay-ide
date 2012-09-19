1c74c0655dd6ab67dc2546d7e8a306c6aba078c6
Gregory Amerson <gregory.amerson@liferay.com>
2012-09-19 17:59:48 +0800

    IDE-700

---
+++ portal/tests/com.liferay.ide.portal.core.tests/src/com/liferay/ide/portal/core/tests/PortalCoreTests.java
@@ -0,0 +1,44 @@
+
+package com.liferay.ide.portal.core.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import com.liferay.ide.core.tests.CoreTests;
+import com.liferay.ide.portal.core.model.StructuresRoot;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.sapphire.modeling.xml.RootXmlResource;
+import org.eclipse.sapphire.modeling.xml.XmlResourceStore;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PortalCoreTests extends CoreTests
+{
+
+    private static final String STRUCTURES_TODO_XML = "structures/todo.xml";
+    private IProject a;
+
+    @Before
+    public void createTestProject() throws Exception
+    {
+        deleteProject( "a" );
+        this.a = createProject( "a" );
+    }
+
+    @Test
+    public void testStructureModelReading() throws Exception
+    {
+        final IFile todoFile =
+            createFile( this.a, STRUCTURES_TODO_XML, this.getClass().getResourceAsStream( STRUCTURES_TODO_XML ) );
+
+        assertEquals( todoFile.getFullPath().lastSegment(), "todo.xml" );
+
+        final StructuresRoot todo =
+            StructuresRoot.TYPE.instantiate( new RootXmlResource( new XmlResourceStore( todoFile.getContents() ) ) );
+        
+        assertEquals( todo.getAvailableLocales().getContent(), "en_US" );
+        assertEquals( todo.getDefaultLocale().getContent(), "en_US" );
+    }
+
+}
