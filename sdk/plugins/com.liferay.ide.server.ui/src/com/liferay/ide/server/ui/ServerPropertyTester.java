b0cf69a02841960f17422bf759dddb928ece316e
Cindy Li <cindy.li@liferay.com>
2012-12-28 10:59:52 +0800

    IDE-762

---
+++ sdk/plugins/com.liferay.ide.server.ui/src/com/liferay/ide/server/ui/ServerPropertyTester.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-2012 Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+package com.liferay.ide.server.ui;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.wst.server.core.IServerAttributes;
+import org.eclipse.wst.server.core.IServerType;
+
+/**
+ * @author Cindy Li
+ */
+public class ServerPropertyTester extends PropertyTester
+{
+    public boolean test( Object receiver, String property, Object[] args, Object expectedValue )
+    {
+        if( receiver instanceof IServerAttributes )
+        {
+            IServerAttributes server = (IServerAttributes) receiver;
+            IServerType serverType = server.getServerType();
+
+            if( serverType != null )
+            {
+                String id = serverType.getId();
+
+                if( "com.liferay.ide.eclipse.server.remote".equals( id ) ) //$NON-NLS-1$
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+}
