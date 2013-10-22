a9930225082f1bb178c751df7e27ea47b2a52dd4
Gregory Amerson <gregory.amerson@liferay.com>
2013-10-22 22:52:43 +0800

    IDE-1190
    Detect liferay-maven-plugin configured under
    <build>
        <pluginManagement>

--- maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/MavenUtil.java
+++ maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/MavenUtil.java
@@ -135,6 +135,13 @@ public class MavenUtil
             retval = mavenProject.getPlugin( ILiferayMavenConstants.LIFERAY_MAVEN_PLUGIN_KEY );
         }
 
+        if( retval == null )
+        {
+            retval =
+                mavenProject.getPluginManagement().getPluginsAsMap().get(
+                    ILiferayMavenConstants.LIFERAY_MAVEN_PLUGIN_KEY );
+        }
+
         return retval;
     }
 
