90b753beaf42c2857c717a1c0b1d0862800f04b0
Gregory Amerson <gregory.amerson@liferay.com>
2014-01-07 12:11:04 +0800

    IDE-1365 upgraded to laut 1.0.4

--- tools/tests/com.liferay.ide.alloy.core.tests/src/com/liferay/ide/alloy/core/tests/AlloyCoreTests.java
+++ tools/tests/com.liferay.ide.alloy.core.tests/src/com/liferay/ide/alloy/core/tests/AlloyCoreTests.java
@@ -7,11 +7,16 @@ import static org.junit.Assert.assertNotNull;
 import com.liferay.ide.alloy.core.AlloyCore;
 import com.liferay.ide.alloy.core.LautRunner;
 import com.liferay.ide.core.tests.BaseTests;
+import com.liferay.ide.core.util.CoreUtil;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,7 +37,7 @@ public class AlloyCoreTests extends BaseTests
 
 
     @Test
-    public void testLautInstallation()
+    public void testLautInstallation() throws Exception
     {
         final LautRunner lautRunner = AlloyCore.getLautRunner();
 
@@ -50,9 +55,32 @@ public class AlloyCoreTests extends BaseTests
     }
 
     @Test
-    public void testUpgrade()
+    public void testUpgrade() throws Exception
     {
+        final IFolder cssFolder = this.a.getFolder( new Path( "docroot/css/" ) );
 
+        CoreUtil.prepareFolder( cssFolder );
+
+        assertEquals( true, cssFolder.exists() );
+
+        final IFile cssFile = cssFolder.getFile( "main.css" );
+
+        final String originalMainCss = CoreUtil.readStreamToString(
+            this.getClass().getResourceAsStream( "files/01_events-display-portlet/docroot/css/main.css" ) );
+
+        final NullProgressMonitor npm = new NullProgressMonitor();
+
+        cssFile.create( new ByteArrayInputStream( originalMainCss.getBytes() ), true, npm );
+
+        assertEquals( true, cssFile.exists() );
+
+        AlloyCore.getLautRunner().exec( a, npm );
+
+        final String expectedMainCss =
+            CoreUtil.readStreamToString( this.getClass().getResourceAsStream(
+                "files/02_events-display-portlet/docroot/css/main.css" ) );
+
+        assertEquals( expectedMainCss, CoreUtil.readStreamToString( cssFile.getContents() ) );
     }
 
 }
