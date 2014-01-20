e5327bee606544ec50ed23e9e8b5a4f5d5354e1e
Simon Jiang <simon.jiang@liferay.com>
2014-01-20 22:45:10 +0800

    IDE-1389
    set false as requires-namespaced-parameters default value in
    liferay-portlet.xml

--- tools/plugins/com.liferay.ide.portlet.jsf.core/src/com/liferay/ide/portlet/jsf/core/operation/JSFPortletDescriptorHelper.java
+++ tools/plugins/com.liferay.ide.portlet.jsf.core/src/com/liferay/ide/portlet/jsf/core/operation/JSFPortletDescriptorHelper.java
@@ -15,14 +15,31 @@
 
 package com.liferay.ide.portlet.jsf.core.operation;
 
+import com.liferay.ide.core.ILiferayConstants;
+import com.liferay.ide.core.ILiferayProject;
+import com.liferay.ide.core.LiferayCore;
+import com.liferay.ide.core.util.CoreUtil;
+import com.liferay.ide.core.util.NodeUtil;
 import com.liferay.ide.portlet.core.dd.PortletDescriptorHelper;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMDocument;
+import org.eclipse.wst.xml.core.internal.provisional.format.FormatProcessorXML;
+import org.osgi.framework.Version;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * @author Greg Amerson
+ * @author Simon Jiang
  */
+
+@SuppressWarnings("restriction")
 public class JSFPortletDescriptorHelper extends PortletDescriptorHelper
     implements INewJSFPortletClassDataModelProperties
 {
@@ -33,9 +50,79 @@ public class JSFPortletDescriptorHelper extends PortletDescriptorHelper
     }
 
     @Override
+    public IStatus addNewPortlet( final IDataModel model )
+    {
+        IStatus status = super.addNewPortlet( model );
+
+        if( !status.isOK() )
+        {
+            return status;
+        }
+
+        ILiferayProject liferayProject = LiferayCore.create( this.project );
+
+        if( liferayProject != null && liferayProject.getPortalVersion() != null )
+        {
+            final Version runtimeVersion = new Version( liferayProject.getPortalVersion() );
+
+            // Runtime version should be equal or greater than 6.2.
+            if( CoreUtil.compareVersions( runtimeVersion, ILiferayConstants.V620 ) >= 0 )
+            {
+                final IFile descriptorFile = getDescriptorFile( ILiferayConstants.LIFERAY_PORTLET_XML_FILE );
+
+                if( descriptorFile != null )
+                {
+                    DOMModelOperation op = new DOMModelEditOperation( descriptorFile )
+                    {
+
+                        @Override
+                        protected void createDefaultFile()
+                        {
+                            // Getting document from super( descriptorFile );
+                        }
+
+                        @Override
+                        protected IStatus doExecute( IDOMDocument document )
+                        {
+                            return updateJSFLiferayPortletXML( document );
+                        }
+                    };
+
+                    IStatus opStatus = op.execute();
+
+                    if( !opStatus.isOK() )
+                    {
+                        return opStatus;
+                    }
+                }
+            }
+        }
+
+        return status;
+    }
+
+    @Override
     protected String getPortletClassText( IDataModel model )
     {
         return model.getStringProperty( JSF_PORTLET_CLASS );
     }
 
+    private IStatus updateJSFLiferayPortletXML( IDOMDocument document )
+    {
+        Element rootElement = document.getDocumentElement();
+
+        NodeList portletNodes = rootElement.getElementsByTagName( "portlet" );
+
+        if( portletNodes.getLength() > 1 )
+        {
+            FormatProcessorXML processor = new FormatProcessorXML();
+            Element lastPortletElement = (Element) portletNodes.item( portletNodes.getLength() - 1 );
+            Node headerPortletClassElement = lastPortletElement.getElementsByTagName( "header-portlet-css" ).item( 0 );
+            NodeUtil.insertChildElement(
+                lastPortletElement, headerPortletClassElement, "requires-namespaced-parameters", "false" );
+            processor.formatNode( lastPortletElement );
+        }
+
+        return Status.OK_STATUS;
+    }
 }
