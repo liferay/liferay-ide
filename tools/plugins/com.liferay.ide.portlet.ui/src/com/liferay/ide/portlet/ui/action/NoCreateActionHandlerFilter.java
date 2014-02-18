7522af8a1c1b20422c8cc635f06cd0ce535089ce
Simon Jiang <simon.jiang@liferay.com>
2014-02-18 17:22:45 +0800

    IDE-1418 Add Sapphire handler check to remove create handler

---
+++ tools/plugins/com.liferay.ide.portlet.ui/src/com/liferay/ide/portlet/ui/action/NoCreateActionHandlerFilter.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.portlet.ui.action;
+
+import org.eclipse.sapphire.ui.SapphireActionHandler;
+import org.eclipse.sapphire.ui.SapphireActionHandlerFilter;
+
+/**
+ * @author Simon Jiang
+ */
+public class NoCreateActionHandlerFilter extends SapphireActionHandlerFilter
+{
+
+    @Override
+    public boolean check( SapphireActionHandler handler )
+    {
+        return !( handler.getId().contains( "Sapphire.Create.Java" ) ); //$NON-NLS-1$
+    }
+
+}
