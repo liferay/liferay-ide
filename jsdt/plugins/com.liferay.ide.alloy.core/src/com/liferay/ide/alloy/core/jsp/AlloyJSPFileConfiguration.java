2db3aa2335081122716307585a73a2a9dc8f350b
Gregory Amerson <gregory.amerson@liferay.com>
2014-09-25 00:07:13 +0800

    IDE-384 adopt new ternFileConfiguration extension point

---
+++ jsdt/plugins/com.liferay.ide.alloy.core/src/com/liferay/ide/alloy/core/jsp/AlloyJSPFileConfiguration.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+package com.liferay.ide.alloy.core.jsp;
+
+import com.liferay.ide.project.core.util.ProjectUtil;
+
+import org.eclipse.core.resources.IFile;
+
+import tern.eclipse.ide.core.ITernFileConfiguration;
+import tern.server.protocol.html.ScriptTagRegion;
+
+
+/**
+ * @author Gregory Amerson
+ */
+public class AlloyJSPFileConfiguration implements ITernFileConfiguration
+{
+    private static final ScriptTagRegion[] tags = new ScriptTagRegion[] { new ScriptTagRegion( "aui:script" ) };
+
+    @Override
+    public ScriptTagRegion[] getScriptTags( IFile file )
+    {
+        if( file != null && ProjectUtil.isPortletProject( file.getProject() ) )
+        {
+            return tags;
+        }
+
+        return null;
+    }
+
+}
