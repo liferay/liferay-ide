3cf9e180d477017bfdae76f63daf05b03848f6dd
Gregory Amerson <gregory.amerson@liferay.com>
2014-10-08 13:24:47 +0800

    IDE-1661 added two unit tests

---
+++ maven/tests/com.liferay.ide.maven.core.tests/src/com/liferay/ide/maven/core/tests/VersionsTests.java
@@ -0,0 +1,62 @@
+/**
+ *
+ */
+
+package com.liferay.ide.maven.core.tests;
+
+import com.liferay.ide.core.ILiferayProject;
+import com.liferay.ide.core.LiferayCore;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+import org.eclipse.m2e.tests.common.RequireMavenExecutionContext;
+import org.junit.Test;
+
+/**
+ * @author Gregory Amerson
+ */
+@SuppressWarnings( "restriction" )
+@RequireMavenExecutionContext
+public class VersionsTests extends AbstractMavenProjectTestCase
+{
+
+    @Test
+    public void testFindLiferayVersionByDeps() throws Exception
+    {
+        IProject project = importProject( "projects/versions/deps-portlet/pom.xml" );
+
+        assertNotNull( project );
+
+        IMavenProjectFacade facade = MavenPlugin.getMavenProjectRegistry().create( project, monitor );
+
+        assertNotNull( facade );
+
+        final ILiferayProject lrproject = LiferayCore.create( project );
+
+        assertNotNull( lrproject );
+
+        assertEquals( "6.2.1", lrproject.getPortalVersion() );
+    }
+
+    @Test
+    public void testFindLiferayVersionByProperties() throws Exception
+    {
+        IProject project = importProject( "projects/versions/properties-portlet/pom.xml" );
+
+        assertNotNull( project );
+
+        IMavenProjectFacade facade = MavenPlugin.getMavenProjectRegistry().create( project, monitor );
+
+        assertNotNull( facade );
+
+        final ILiferayProject lrproject = LiferayCore.create( project );
+
+        assertNotNull( lrproject );
+
+        assertEquals( "6.2.1", lrproject.getPortalVersion() );
+    }
+
+
+}
