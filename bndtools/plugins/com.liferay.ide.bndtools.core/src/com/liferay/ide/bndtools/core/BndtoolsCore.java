34d39a535eac39ce2233b81bf1f98bddc435bc6a
Gregory Amerson <gregory.amerson@liferay.com>
2015-01-07 12:19:01 +0800

    Added bndtools.core with support for IBundleProject

---
+++ bndtools/plugins/com.liferay.ide.bndtools.core/src/com/liferay/ide/bndtools/core/BndtoolsCore.java
@@ -0,0 +1,109 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.bndtools.core;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plugin life cycle
+ *
+ * @author Gregory Amerson
+ */
+public class BndtoolsCore extends Plugin
+{
+
+    // The shared instance
+    private static BndtoolsCore plugin;
+
+    // The plugin ID
+    public static final String PLUGIN_ID = "com.liferay.ide.bndtools.core"; //$NON-NLS-1$
+
+    public static IStatus createErrorStatus( Exception ex )
+    {
+        return new Status( IStatus.ERROR, PLUGIN_ID, ex.getMessage(), ex );
+    }
+
+    public static IStatus createErrorStatus( String msg )
+    {
+        return new Status( IStatus.ERROR, PLUGIN_ID, msg );
+    }
+
+    public static IStatus createErrorStatus( String msg, Exception e )
+    {
+        return new Status( IStatus.ERROR, PLUGIN_ID, msg, e );
+    }
+
+    public static IStatus createWarningStatus( String msg )
+    {
+        return new Status( IStatus.WARNING, PLUGIN_ID, msg );
+    }
+
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static BndtoolsCore getDefault()
+    {
+        return plugin;
+    }
+
+
+    public static void logError( Exception ex )
+    {
+        getDefault().getLog().log( createErrorStatus( ex ) );
+    }
+
+    public static void logError( String msg )
+    {
+        getDefault().getLog().log( createErrorStatus( msg ) );
+    }
+
+    public static void logError( String msg, Exception e )
+    {
+        getDefault().getLog().log( createErrorStatus( msg, e ) );
+    }
+
+    /**
+     * The constructor
+     */
+    public BndtoolsCore()
+    {
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start( BundleContext context ) throws Exception
+    {
+        super.start( context );
+        plugin = this;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop( BundleContext context ) throws Exception
+    {
+        plugin = null;
+        super.stop( context );
+    }
+}
---
+++ bndtools/plugins/com.liferay.ide.bndtools.core/src/com/liferay/ide/bndtools/core/BndtoolsProvider.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+package com.liferay.ide.bndtools.core;
+
+import aQute.bnd.build.Project;
+import bndtools.central.Central;
+
+import com.liferay.ide.core.AbstractLiferayProjectProvider;
+import com.liferay.ide.core.ILiferayProject;
+import com.liferay.ide.core.ILiferayProjectProvider;
+
+import org.eclipse.core.resources.IProject;
+
+
+/**
+ * @author Gregory Amerson
+ */
+public class BndtoolsProvider extends AbstractLiferayProjectProvider implements ILiferayProjectProvider
+{
+
+    public BndtoolsProvider()
+    {
+        super( new Class<?>[] { IProject.class } );
+    }
+
+    @Override
+    public ILiferayProject provide( Object adaptable )
+    {
+        ILiferayProject retval = null;
+
+        if( adaptable instanceof IProject )
+        {
+            final IProject project = (IProject) adaptable;
+
+            try
+            {
+                Project bndProject = Central.getProject( project );
+
+                if( bndProject != null )
+                {
+                    retval = new BndtoolsProject( project, bndProject );
+                }
+            }
+            catch( Exception e )
+            {
+            }
+        }
+
+        return retval;
+    }
+
+}
