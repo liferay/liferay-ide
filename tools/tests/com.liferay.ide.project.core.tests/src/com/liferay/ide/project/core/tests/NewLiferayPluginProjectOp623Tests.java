3316a588e2ad21a0ab19f7c657bf932694cba2c6
Terry Jia <terry.jia@liferay.com>
2015-05-19 13:43:02 +0800

    IDE-1963 add the 6.2.3 unit tests

---
+++ tools/tests/com.liferay.ide.project.core.tests/src/com/liferay/ide/project/core/tests/NewLiferayPluginProjectOp623Tests.java
@@ -0,0 +1,169 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.core.tests;
+
+import com.liferay.ide.project.core.ProjectCore;
+
+import org.eclipse.core.runtime.IPath;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Terry Jia
+ */
+public class NewLiferayPluginProjectOp623Tests extends NewLiferayPluginProjectOpBase
+{
+
+    @Override
+    protected IPath getLiferayPluginsSdkDir()
+    {
+        return ProjectCore.getDefault().getStateLocation().append( "liferay-plugins-sdk-6.2" );
+    }
+
+    @Override
+    protected IPath getLiferayPluginsSDKZip()
+    {
+        return getLiferayBundlesPath().append( "liferay-plugins-sdk-6.2-ce-ga4-20150416163831865.zip" );
+    }
+
+    @Override
+    protected String getLiferayPluginsSdkZipFolder()
+    {
+        return "liferay-plugins-sdk-6.2/";
+    }
+
+    @Override
+    protected IPath getLiferayRuntimeDir()
+    {
+        return ProjectCore.getDefault().getStateLocation().append( "liferay-portal-6.2.0-ce-ga1/tomcat-7.0.42" );
+    }
+
+    @Override
+    protected IPath getLiferayRuntimeZip()
+    {
+        return getLiferayBundlesPath().append( "liferay-portal-tomcat-6.2.0-ce-ga1-20131101192857659.zip" );
+    }
+
+    @Override
+    protected String getRuntimeId()
+    {
+        return "com.liferay.ide.eclipse.server.tomcat.runtime.70";
+    }
+
+    @Override
+    public String getRuntimeVersion()
+    {
+        return "6.2.3";
+    }
+
+    @Override
+    protected String getServiceXmlDoctype()
+    {
+        return "service-builder PUBLIC \"-//Liferay//DTD Service Builder 6.2.0//EN\" \"http://www.liferay.com/dtd/liferay-service-builder_6_2_0.dtd";
+    }
+
+    @Test
+    public void testLocationListener() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testLocationListener();
+    }
+
+    @Test
+    public void testNewJsfRichfacesProjects() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testNewJsfRichfacesProjects();
+    }
+
+    @Test
+    public void testNewLayoutAntProject() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testNewLayoutAntProject();
+    }
+
+    @Test
+    @Ignore
+    public void testNewProjectCustomLocationPortlet() throws Exception
+    {
+        // not supported in 6.2.3
+    }
+
+    @Test
+    @Ignore
+    public void testNewProjectCustomLocationWrongSuffix() throws Exception
+    {
+        // not supported in 6.2.3
+    }
+
+    @Test
+    @Ignore
+    public void testNewSDKProjectCustomLocation() throws Exception
+    {
+        // not supported in 6.2.3
+    }
+
+    @Test
+    @Ignore
+    public void testNewSDKProjectEclipseWorkspace() throws Exception
+    {
+        // not supported in 6.2.3
+    }
+
+    @Test
+    public void testNewSDKProjectInSDK() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testNewSDKProjectInSDK();
+    }
+
+    @Test
+    public void testNewThemeProjects() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testNewThemeProjects();
+    }
+
+    @Test
+    public void testPluginTypeListener() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testPluginTypeListener( true );
+    }
+
+    @Test
+    public void testProjectNameValidation() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testProjectNameValidation( "project-name-validation-623" );
+    }
+
+    @Test
+    public void testUseSdkLocationListener() throws Exception
+    {
+        if( shouldSkipBundleTests() ) return;
+
+        super.testUseSdkLocationListener();
+    }
+}
