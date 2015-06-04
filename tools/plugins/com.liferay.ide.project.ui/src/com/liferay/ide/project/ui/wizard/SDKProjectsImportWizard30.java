4814033b174e5713472ce439f8416afd16c1abff
simon.jiang <simon.jiang@liferay.com>
2015-06-04 15:56:52 +0800

    IDE-1962 Create new import sdk project wizard

---
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/wizard/SDKProjectsImportWizard30.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+package com.liferay.ide.project.ui.wizard;
+
+import com.liferay.ide.core.util.CoreUtil;
+import com.liferay.ide.project.core.model.SDKProjectsImportOp30;
+
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.sapphire.ui.def.DefinitionLoader;
+import org.eclipse.sapphire.ui.forms.swt.SapphireWizard;
+import org.eclipse.sapphire.ui.forms.swt.SapphireWizardPage;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWizard;
+
+
+/**
+ * @author Simon Jiang
+ */
+
+public class SDKProjectsImportWizard30 extends SapphireWizard<SDKProjectsImportOp30>
+    implements IWorkbenchWizard, INewWizard
+{
+    private boolean firstErrorMessageRemoved = false;
+
+    public SDKProjectsImportWizard30()
+    {
+        super( createDefaultOp(), DefinitionLoader.sdef( SDKProjectsImportWizard30.class ).wizard() );
+    }
+
+    @Override
+    public IWizardPage[] getPages()
+    {
+        final IWizardPage[] wizardPages = super.getPages();
+
+        if( !firstErrorMessageRemoved && wizardPages != null )
+        {
+            final SapphireWizardPage wizardPage = (SapphireWizardPage) wizardPages[0];
+
+            final String message = wizardPage.getMessage();
+            final int messageType = wizardPage.getMessageType();
+
+            if( messageType == IMessageProvider.ERROR && ! CoreUtil.isNullOrEmpty( message ) )
+            {
+                //wizardPage.setMessage( "Please select .", SapphireWizardPage.NONE ); //$NON-NLS-1$
+                firstErrorMessageRemoved = true;
+            }
+        }
+
+        return wizardPages;
+    }
+
+    public void init( IWorkbench workbench, IStructuredSelection selection )
+    {
+    }
+
+
+    @Override
+    protected void performPostFinish()
+    {
+    }
+
+
+    private static SDKProjectsImportOp30 createDefaultOp()
+    {
+        return SDKProjectsImportOp30.TYPE.instantiate();
+    }
+
+}
