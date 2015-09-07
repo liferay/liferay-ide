ba229849df317dbb6d9346698eae8bf442e53033
Gregory Amerson <gregory.amerson@liferay.com>
2015-09-07 15:39:31 -0500

    IDE-2007 switch back to local version of JMXBundleDeployer with support for WAB deployment

--- tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/BundleDeployer.java
+++ tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/BundleDeployer.java
@@ -14,12 +14,14 @@
  *******************************************************************************/
 package com.liferay.ide.server.core.portal;
 
-import aQute.remote.util.JMXBundleDeployer;
-
-import java.lang.reflect.Field;
+import java.io.IOException;
+import java.util.Set;
 
 import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
+
+import org.osgi.framework.dto.BundleDTO;
 
 
 /**
@@ -28,21 +30,88 @@ import javax.management.ObjectName;
 public class BundleDeployer extends JMXBundleDeployer
 {
 
+    private final static String OBJECTNAME = "osgi.core";
+
     public BundleDeployer( int jmxRemotePort )
     {
         super( jmxRemotePort );
+    }
+
+    /**
+     * Gets the current list of installed bsns, compares it to the bsn provided.
+     * If bsn doesn't exist, then install it. If it does exist then update it.
+     *
+     * @param bsn
+     *            Bundle-SymbolicName of bundle you are wanting to deploy
+     * @param bundle
+     *            the bundle
+     * @return the id of the updated or installed bundle
+     * @throws Exception
+     */
+    public long deploy(String bsn, String bundleUrl) throws Exception {
+        MBeanServerConnection connection = mBeanServerConnection;
+
+        final ObjectName framework = getFramework(connection);
+
+        long bundleId = -1;
+
+        for (BundleDTO osgiBundle : listBundles()) {
+            if (osgiBundle.symbolicName.equals(bsn)) {
+                bundleId = osgiBundle.id;
+                break;
+            }
+        }
+
+        // TODO serve bundle url over http so it works for non file:// urls
+
+        if (bundleId > -1) {
+            connection.invoke(framework, "stopBundle",
+                    new Object[] { bundleId }, new String[] { "long" });
+
+            connection.invoke(framework, "updateBundleFromURL",
+                    new Object[] { bundleId,
+                    bundleUrl },
+                    new String[] { "long", String.class.getName() });
+
+            connection.invoke(framework, "refreshBundle",
+                    new Object[] { bundleId }, new String[] { "long" });
+        } else {
+            Object installed = connection.invoke(
+                    framework,
+                    "installBundleFromURL",
+                    new Object[] { bundleUrl, bundleUrl },
+                    new String[] { String.class.getName(),
+                            String.class.getName() });
+
+            bundleId = Long.parseLong(installed.toString());
+        }
+
+        connection.invoke(framework, "startBundle",
+            new Object[] { bundleId }, new String[] { "long" });
+
+        return bundleId;
+    }
+
+    private static ObjectName getFramework( MBeanServerConnection mBeanServerConnection )
+        throws MalformedObjectNameException, IOException
+    {
+        final ObjectName objectName = new ObjectName( OBJECTNAME + ":type=framework,*" );
+        final Set<ObjectName> objectNames = mBeanServerConnection.queryNames( objectName, null );
+
+        if( objectNames != null && objectNames.size() > 0 )
+        {
+            return objectNames.iterator().next();
+        }
+
+        return null;
     }
 
     public boolean ping()
     {
         try
         {
-            Field connectionField = this.getClass().getField( "mBeanServerConnection" );
-            connectionField.setAccessible( true );
-            MBeanServerConnection connection = (MBeanServerConnection) connectionField.get( this );
-
-            return connection != null &&
-                connection.queryNames( new ObjectName( "osgi.core:type=bundleState,*" ), null ) != null;
+            return mBeanServerConnection != null &&
+                            mBeanServerConnection.queryNames( new ObjectName( "osgi.core:type=bundleState,*" ), null ) != null;
         }
         catch( Exception e )
         {
