673ea746fe724216aed6c21522c3489f0c943efd
Andy Wu <andy.wu@liferay.com>
2015-10-14 09:28:35 +0800

    IDE-2105 tree view sort the folders first before files

---
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/migration/MigrationViewSorter.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.ui.migration;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.jface.viewers.ViewerSorter;
+
+/**
+ * @author Andy Wu
+ */
+public class MigrationViewSorter extends ViewerSorter
+{
+    @Override
+    public int category( Object element )
+    {
+        if( element instanceof MPNode )
+        {
+            return -1;
+        }
+        else
+        {
+            return super.category( element );
+        }
+    }
+
+}
