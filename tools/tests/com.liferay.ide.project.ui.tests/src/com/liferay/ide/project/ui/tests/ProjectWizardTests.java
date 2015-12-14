94b927465db4e4bc39188968830af43483deda61
Terry Jia <terry.jia@liferay.com>
2015-12-14 14:16:23 +0800

    IDE-2143 SF

--- tools/tests/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/ProjectWizardTests.java
+++ tools/tests/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/ProjectWizardTests.java
@@ -135,10 +135,11 @@ public class ProjectWizardTests extends SWTBotBase implements ProjectWizard
         if( added )
         {
             page1.finish();
-//            sleep(5000);
-//            assertTrue((treeUtil.expandNode( "textsb-portlet","docroot").getNode("view.jsp").isVisible()));
-//            assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","css").getNode("main.css").isVisible()));
-//            assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","js").getNode("main.js").isVisible()));
+
+            // sleep(5000);
+            // assertTrue((treeUtil.expandNode( "textsb-portlet","docroot").getNode("view.jsp").isVisible()));
+            // assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","css").getNode("main.css").isVisible()));
+            // assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","js").getNode("main.js").isVisible()));
         }
         else
         {
@@ -146,10 +147,11 @@ public class ProjectWizardTests extends SWTBotBase implements ProjectWizard
             SetSDKLocationPageObject<SWTWorkbenchBot> page2 = new SetSDKLocationPageObject<SWTWorkbenchBot>( bot, "" );
             page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
             page2.finish();
-//            sleep(5000);
-//            assertTrue((treeUtil.expandNode( "textsb-portlet","docroot").getNode("view.jsp").isVisible()));
-//            assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","css").getNode("main.css").isVisible()));
-//            assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","js").getNode("main.js").isVisible()));
+
+            // sleep(5000);
+            // assertTrue((treeUtil.expandNode( "textsb-portlet","docroot").getNode("view.jsp").isVisible()));
+            // assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","css").getNode("main.css").isVisible()));
+            // assertTrue((treeUtil.expandNode( "textsb-portlet","docroot","js").getNode("main.js").isVisible()));
         }
     }
 
@@ -344,10 +346,13 @@ public class ProjectWizardTests extends SWTBotBase implements ProjectWizard
         else
         {
             page1.next();
-            SetSDKLocationPageObject<SWTWorkbenchBot> page2 = new SetSDKLocationPageObject<SWTWorkbenchBot>( bot, "", INDEX_VALIDATION_MESSAGE2 );
+            SetSDKLocationPageObject<SWTWorkbenchBot> page2 = new SetSDKLocationPageObject<SWTWorkbenchBot>(
+                bot, "", INDEX_VALIDATION_MESSAGE2 );
+
             page2.setSdkLocation( getLiferayPluginsSdkDir().toString() );
+
             sleep( 1500 );
-        	assertEquals( TEXT_WEB_SDK_62_ERRORR_MESSAGE, page2.getValidationMessage() );
+            assertEquals( TEXT_WEB_SDK_62_ERRORR_MESSAGE, page2.getValidationMessage() );
             page2.cancel();
         }
     }
--- tools/tests/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/SetSDKLocationPageObject.java
+++ tools/tests/com.liferay.ide.project.ui.tests/src/com/liferay/ide/project/ui/tests/SetSDKLocationPageObject.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
 
 package com.liferay.ide.project.ui.tests;
 
@@ -6,24 +20,27 @@ import com.liferay.ide.ui.tests.swtbot.page.WizardPageObject;
 
 import org.eclipse.swtbot.swt.finder.SWTBot;
 
-public class SetSDKLocationPageObject<T extends SWTBot> extends WizardPageObject<T>
+/**
+ * @author Terry Jia
+ * @author Ying Xu
+ */
+public class SetSDKLocationPageObject<T extends SWTBot> extends WizardPageObject<T> implements ProjectWizard
 {
 
     TextPageObject<SWTBot> sdkLocationText;
 
     public SetSDKLocationPageObject( T bot, String title )
     {
-        super( bot, title, "Cancel", "Finish", "< Back", "Next >" );
-
-        sdkLocationText = new TextPageObject<SWTBot>( bot, "SDK Location:" );
+        this( bot, title, -1 );
     }
 
     public SetSDKLocationPageObject( T bot, String title, int index )
     {
-        super( bot, title, "Cancel", "Finish", "< Back", "Next >", index );
+        super( bot, title, BUTTON_CANCEL, BUTTON_FINISH, BUTTON_BACK, BUTTON_NEXT, index );
 
         sdkLocationText = new TextPageObject<SWTBot>( bot, "SDK Location:" );
     }
+
     public void setSdkLocation( String sdkLocation )
     {
         sdkLocationText.setText( sdkLocation );
