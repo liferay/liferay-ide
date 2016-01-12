d57b64c82db51d1cdce71b7f993bc93a4919579e
Lovett Li <lovett.li@liferay.com>
2016-01-12 11:23:26 +0800

    IDE-2155 Add function to dynamically get liferay extension point

---
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/modules/ServiceCommand.java
@@ -0,0 +1,267 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.core.modules;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.IStatus;
+
+import com.liferay.ide.server.util.SocketUtil;
+
+import aQute.remote.api.Agent;
+import aQute.remote.api.Event;
+import aQute.remote.api.Supervisor;
+import aQute.remote.util.AgentSupervisor;
+
+/**
+ * @author Lovett Li
+ */
+public class ServiceCommand
+{
+    private String _serviceName;
+
+    public ServiceCommand()
+    {
+    }
+
+    public ServiceCommand( String serviceName )
+    {
+        _serviceName = serviceName;
+    }
+
+    public String[] execute() throws Exception
+    {
+        ServicesSupervisor supervisor = null;
+        String[] result = null;
+
+        try
+        {
+            supervisor = connectRemote();
+
+            if( _serviceName == null )
+            {
+                result = getServices( supervisor );
+            }
+            else
+            {
+                result = getServiceBundle( _serviceName, supervisor );
+            }
+
+            return result;
+        }
+        finally
+        {
+            if( supervisor != null )
+            {
+                supervisor.close();
+            }
+        }
+    }
+
+    private ServicesSupervisor connectRemote() throws Exception
+    {
+        IStatus status = SocketUtil.canConnect( "localhost", String.valueOf( Agent.DEFAULT_PORT ) );
+
+        if( !( status != null && status.isOK() ) )
+        {
+            return null;
+        }
+
+        ServicesSupervisor supervisor = new ServicesSupervisor();
+        supervisor.connect( "localhost", Agent.DEFAULT_PORT );
+
+        if( !supervisor.getAgent().redirect( -1 ) )
+        {
+            supervisor.close();
+
+            return null;
+        }
+
+        return supervisor;
+    }
+
+    private String[] getServices( ServicesSupervisor supervisor ) throws Exception
+    {
+        supervisor.getAgent().stdin( "services" );
+
+        return parseService( supervisor.getOutInfo() );
+    }
+
+    private String[] getServiceBundle( String serviceName, ServicesSupervisor supervisor ) throws Exception
+    {
+        String[] serviceBundleInfo;
+
+        supervisor.getAgent().stdin( "packages " + serviceName.substring( 0, serviceName.lastIndexOf( "." ) ) );
+
+        if( supervisor.getOutInfo().equals( "No exported packages\n" ) )
+        {
+            supervisor.getAgent().stdin(
+                "services " + "(objectClass=" + serviceName + ")" + " | grep \"Registered by bundle:\" " );
+            serviceBundleInfo = parseRegisteredBundle( supervisor.getOutInfo() );
+        }
+        else
+        {
+            serviceBundleInfo = parseSymbolicName( supervisor.getOutInfo() );
+        }
+
+        return serviceBundleInfo;
+    }
+
+    public class ServicesSupervisor extends AgentSupervisor<Supervisor, Agent> implements Supervisor
+    {
+        private String outinfo;
+
+        @Override
+        public boolean stdout( String out ) throws Exception
+        {
+            if( !"".equals( out ) && out != null )
+            {
+                out = out.replaceAll( ".*>.*$", "" );
+
+                if( !out.equals( "" ) && !out.equals( "true\n" ) && !out.equals( "false\n" ) )
+                {
+                    outinfo = out;
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        public boolean stderr( String out ) throws Exception
+        {
+            return true;
+        }
+
+        public void connect( String host, int port ) throws Exception
+        {
+            super.connect( Agent.class, this, host, port );
+        }
+
+        @Override
+        public void event( Event e ) throws Exception
+        {
+        }
+
+        public String getOutInfo()
+        {
+            return outinfo;
+        }
+
+    }
+
+    private String[] parseService( String outinfo )
+    {
+        final Pattern pattern = Pattern.compile( "(?<=\\{)(.+?)(?=\\})" );
+        final Matcher matcher = pattern.matcher( outinfo );
+        final List<String> ls = new ArrayList<>();
+
+        while( matcher.find() )
+        {
+            ls.add( matcher.group() );
+        }
+
+        Iterator<String> iterator = ls.iterator();
+
+        while( iterator.hasNext() )
+        {
+            iterator.next();
+
+            if( iterator.hasNext() )
+            {
+                iterator.next();
+                iterator.remove();
+            }
+        }
+
+        final List<String> listservice = new ArrayList<>();
+
+        for( String bs : ls )
+        {
+            if( bs.split( "," ).length > 1 )
+            {
+                for( String bbs : bs.split( "," ) )
+                {
+                    listservice.add( bbs.trim() );
+                }
+            }
+            else
+            {
+                listservice.add( bs );
+            }
+        }
+
+        final Set<String> set = new HashSet<String>();
+        final List<String> newList = new ArrayList<>();
+
+        for( Iterator<String> iter = listservice.iterator(); iter.hasNext(); )
+        {
+            String element = iter.next();
+
+            if( set.add( element ) )
+            {
+                newList.add( element );
+            }
+        }
+
+        return newList.toArray( new String[0] );
+    }
+
+    private String[] parseRegisteredBundle( String serviceName )
+    {
+        String str = serviceName.substring( 0, serviceName.indexOf( "[" ) );
+        str = str.replaceAll( "\"Registered by bundle:\"", "" ).trim();
+        String[] result = str.split( "_" );
+
+        if( result.length == 2 )
+        {
+            return result;
+        }
+
+        return null;
+    }
+
+    private String[] parseSymbolicName( String info )
+    {
+        final int symbolicIndex = info.indexOf( "bundle-symbolic-name" );
+        final int versionIndex = info.indexOf( "version:Version" );
+        String symbolicName = info.substring( symbolicIndex, info.indexOf( ";", symbolicIndex ) );
+        String version = info.substring( versionIndex, info.indexOf( ";", versionIndex ) );
+
+        final Pattern p = Pattern.compile( "\"([^\"]*)\"" );
+        Matcher m = p.matcher( symbolicName );
+
+        while( m.find() )
+        {
+            symbolicName = m.group( 1 );
+        }
+
+        m = p.matcher( version );
+
+        while( m.find() )
+        {
+            version = m.group( 1 );
+        }
+
+        return new String[] { symbolicName, version };
+    }
+}
---
+++ tools/tests/com.liferay.ide.project.core.tests/src/com/liferay/ide/project/core/tests/modules/ServiceCommandTests.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.core.tests.modules;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.eclipse.core.runtime.IStatus;
+
+import aQute.remote.api.Agent;
+
+import org.junit.Test;
+
+import com.liferay.ide.project.core.modules.ServiceCommand;
+import com.liferay.ide.server.util.SocketUtil;
+
+/**
+ * @author Lovett Li
+ */
+public class ServiceCommandTests
+{
+
+    @Test
+    public void GetIntegrationPoints() throws Exception
+    {
+        if( canConnect() )
+        {
+            String[] IntegrationPoints = new ServiceCommand().execute();
+
+            assertNotNull( IntegrationPoints );
+
+            assertTrue( IntegrationPoints.length > 0 );
+        }
+    }
+
+    @Test
+    public void GetServiceBundle() throws Exception
+    {
+        if( canConnect() )
+        {
+            String[] serviceBundle =
+                new ServiceCommand( "com.liferay.bookmarks.service.BookmarksEntryLocalService" ).execute();
+            String[] serviceBundleNoExportPackage =
+                new ServiceCommand( "com.liferay.site.teams.web.upgrade.SiteTeamsWebUpgrade" ).execute();
+
+            assertEquals( "com.liferay.bookmarks.api", serviceBundle[0] );
+            assertEquals( "1.0.0", serviceBundle[1] );
+
+            assertEquals( "com.liferay.site.teams.web", serviceBundleNoExportPackage[0] );
+            assertEquals( "1.0.0", serviceBundleNoExportPackage[1] );
+        }
+    }
+
+    public boolean canConnect()
+    {
+        IStatus status = SocketUtil.canConnect( "localhost", String.valueOf( Agent.DEFAULT_PORT ) );
+
+        if( status != null && status.isOK() )
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
