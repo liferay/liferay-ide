5595793325fdd23f91e7d07c2d9038fb9ae3e63d
Joye Luo <joye.luo@liferay.com>
2016-12-21 13:54:42 +0800

    IDE-3051 fix maven module project build didn't compile java files

--- maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/MavenBundlePluginProject.java
+++ maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/MavenBundlePluginProject.java
@@ -29,6 +29,7 @@ import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -99,6 +100,9 @@ public class MavenBundlePluginProject extends LiferayMavenProject implements IBu
     public IPath getOutputBundle( boolean cleanBuild, IProgressMonitor monitor ) throws CoreException
     {
         IPath outputJar = null;
+
+        // make sure maven mojos have executed
+        this.getProject().build( IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor );
 
         final MavenProjectBuilder mavenProjectBuilder = new MavenProjectBuilder( this.getProject() );
 
