2f4ceed9c204a2abf03386ec270f7fb2b1c5c411
Gregory Amerson <gregory.amerson@liferay.com>
2016-12-21 09:52:45 -0600

    make our own bnd project configurator secondary to bnd's

--- maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/BundleProjectConfigurator.java
+++ maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/BundleProjectConfigurator.java
@@ -18,17 +18,13 @@ package com.liferay.ide.maven.core;
 import com.liferay.ide.core.LiferayNature;
 
 import org.apache.maven.model.Plugin;
-import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
-import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
-import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 import org.eclipse.m2e.jdt.IClasspathDescriptor;
 import org.eclipse.m2e.jdt.IJavaProjectConfigurator;
@@ -54,13 +50,6 @@ public class BundleProjectConfigurator extends AbstractProjectConfigurator imple
         ProjectConfigurationRequest request, IClasspathDescriptor classpath, IProgressMonitor monitor )
         throws CoreException
     {
-    }
-
-    @Override
-    public AbstractBuildParticipant getBuildParticipant(
-        IMavenProjectFacade projectFacade, MojoExecution execution, IPluginExecutionMetadata executionMetadata )
-    {
-        return new MojoExecutionBuildParticipant( execution, true, true );
     }
 
     @Override
