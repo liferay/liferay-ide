7a1dd4d444e0f2abae535c334d284bfa7c34651c
Terry Jia <terry.jia@liferay.com>
2016-12-27 18:47:10 +0800

    IDE-3057

---
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/handlers/OpenLinkHandler.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.project.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.program.Program;
+
+/**
+ * @author Terry Jia
+ */
+public abstract class OpenLinkHandler extends AbstractHandler
+{
+
+    @Override
+    public Object execute( ExecutionEvent event ) throws ExecutionException
+    {
+        Program.launch( getLink() );
+
+        return null;
+    }
+
+    protected abstract String getLink();
+
+}
