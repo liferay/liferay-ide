2b5f8ee942f689b25a3718d28b249b3d8c296796
Ashley Yuan <yanan.yuan@liferay.com>
2017-01-24 10:49:46 +0800

    Fix blade cli jar test

--- tools/tests/com.liferay.ide.project.core.tests/src/com/liferay/ide/project/core/tests/modules/BladeCLIPrefsOverrideTests.java
+++ tools/tests/com.liferay.ide.project.core.tests/src/com/liferay/ide/project/core/tests/modules/BladeCLIPrefsOverrideTests.java
@@ -67,7 +67,7 @@ public class BladeCLIPrefsOverrideTests
 
         assertTrue( path.toFile().exists() );
 
-        assertTrue( path.toFile().getName().contains( "blade.cli_2.0.1" ) );
+        assertTrue( path.toFile().getName().contains( "blade.cli_2.0.2" ) );
     }
 
     @Test
