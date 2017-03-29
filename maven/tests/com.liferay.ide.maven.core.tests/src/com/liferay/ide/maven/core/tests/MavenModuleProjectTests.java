5237f6c2457b37cfb22de31efb7c6e41d6f16772
Gregory Amerson <gregory.amerson@liferay.com>
2017-03-29 10:20:10 -0500

    IDE-3166 undo test fix since original problem is fixed now

--- maven/tests/com.liferay.ide.maven.core.tests/src/com/liferay/ide/maven/core/tests/MavenModuleProjectTests.java
+++ maven/tests/com.liferay.ide.maven.core.tests/src/com/liferay/ide/maven/core/tests/MavenModuleProjectTests.java
@@ -593,7 +593,7 @@ public class MavenModuleProjectTests extends AbstractMavenProjectTestCase
 
        final String deployedName = projectComponent.getDeployedName();
 
-       assertEquals( "maven-theme-component-test-1.0.0-SNAPSHOT", deployedName );
+       assertEquals( "maven-theme-component-test", deployedName );
     }
 
     @Test
