9328cf800abd13a4b9fe41899a0679aa733b8a84
Andy Wu <andy.wu@liferay.com>
2017-06-02 09:14:37 +0800

    IDE-3266 add welcome page content

---
+++ enterprise/plugins/com.liferay.ide.studio.ui/src/com/liferay/ide/studio/ui/ImportLiferayWorkspaceAction.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the End User License
+ * Agreement for Liferay Developer Studio ("License"). You may not use this file
+ * except in compliance with the License. You can obtain a copy of the License
+ * by contacting Liferay, Inc. See the License for the specific language
+ * governing permissions and limitations under the License, including but not
+ * limited to distribution rights of the Software.
+ */
+
+package com.liferay.ide.studio.ui;
+
+import com.liferay.ide.project.core.workspace.ImportLiferayWorkspaceOp;
+import com.liferay.ide.project.ui.workspace.ImportLiferayWorkspaceWizard;
+
+import java.util.Properties;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.internal.intro.impl.IntroPlugin;
+import org.eclipse.ui.intro.IIntroSite;
+import org.eclipse.ui.intro.config.IIntroAction;
+
+/**
+ * @author Andy Wu
+ */
+@SuppressWarnings( "restriction" )
+public class ImportLiferayWorkspaceAction implements IIntroAction
+{
+
+    @Override
+    public void run( IIntroSite site, Properties params )
+    {
+        ImportLiferayWorkspaceWizard wizard = new ImportLiferayWorkspaceWizard();
+
+        IPath path = new Path( Platform.getInstallLocation().getURL().getFile() );
+
+        ImportLiferayWorkspaceOp op = wizard.element().nearest( ImportLiferayWorkspaceOp.class );
+
+        op.setWorkspaceLocation( path.append( "../liferay-workspace" ).toPortableString() );
+
+        WizardDialog dialog = new WizardDialog( site.getShell(), wizard );
+
+        if( dialog.open() == Window.OK )
+        {
+            IntroPlugin.closeIntro();
+        }
+    }
+
+}
