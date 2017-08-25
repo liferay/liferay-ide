304bf7699025dfb25db537a9b44a233c1a522c0a
Joye Luo <joye.luo@liferay.com>
2017-08-25 10:47:24 +0800

    IDE-3137 fix Japanese characters garbled in workflow

--- enterprise/plugins/com.liferay.ide.kaleo.ui/src/com/liferay/ide/kaleo/ui/AbstractKaleoEditorHelper.java
+++ enterprise/plugins/com.liferay.ide.kaleo.ui/src/com/liferay/ide/kaleo/ui/AbstractKaleoEditorHelper.java
@@ -16,6 +16,7 @@ import static com.liferay.ide.core.util.CoreUtil.empty;
 import com.liferay.ide.core.util.CoreUtil;
 import com.liferay.ide.kaleo.ui.editor.HiddenFileEditorInput;
 import com.liferay.ide.kaleo.ui.editor.ScriptPropertyEditorInput;
+import com.liferay.ide.kaleo.core.KaleoCore;
 import com.liferay.ide.kaleo.core.model.Node;
 
 import java.io.ByteArrayInputStream;
@@ -31,6 +32,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.JavaCore;
@@ -125,10 +127,12 @@ public abstract class AbstractKaleoEditorHelper implements IKaleoEditorHelper
             {
                 tempFile.create( source, true, null );
             }
+
+            tempFile.setCharset( "UTF-8", new NullProgressMonitor() );
         }
         catch( CoreException | UnsupportedEncodingException e )
         {
-            e.printStackTrace();
+            KaleoCore.logError( e );
         }
 
         return tempFile;
