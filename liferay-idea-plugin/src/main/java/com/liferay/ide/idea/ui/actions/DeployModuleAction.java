95c4d04aa306ce439a1a1ca324ed8c08a7759c1c
Andy Wu <andy.wu@liferay.com>
2017-09-14 11:31:47 +0800

    add deploy and initBundle action

---
+++ liferay-idea-plugin/src/main/java/com/liferay/ide/idea/ui/actions/DeployModuleAction.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.idea.ui.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import com.liferay.ide.idea.ui.LiferayIdeaUI;
+
+/**
+ * @author Andy Wu
+ */
+public class DeployModuleAction extends AbstractLiferayGradleTaskAction {
+
+	public DeployModuleAction() {
+		super("Deploy", "Run deploy task", LiferayIdeaUI.LIFERAY_ICON);
+	}
+
+	@Override
+	public boolean isVisible(AnActionEvent e) {
+		if (getVirtualFile(e) != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getTaskName() {
+		return "deploy";
+	}
+
+	@Override
+	protected String getWorkDirectory(AnActionEvent e) {
+		VirtualFile virtualFile = getVirtualFile(e);
+
+		ProjectRootManager projectRootManager = ProjectRootManager.getInstance(e.getProject());
+
+		Module module = projectRootManager.getFileIndex().getModuleForFile(virtualFile);
+
+		ModuleRootManager moduleRootManager = ModuleRootManager.getInstance(module);
+
+		VirtualFile[] roots = moduleRootManager.getModifiableModel().getContentRoots();
+
+		assert roots != null && roots[0] != null;
+
+		return roots[0].getCanonicalPath();
+	}
+
+}
---
+++ liferay-idea-plugin/src/main/java/com/liferay/ide/idea/ui/actions/InitBundleAction.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.idea.ui.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+
+import com.liferay.ide.idea.ui.LiferayIdeaUI;
+
+/**
+ * @author Andy Wu
+ */
+public class InitBundleAction extends AbstractLiferayGradleTaskAction {
+
+	public InitBundleAction() {
+		super("InitBundle", "Run initBundle task", LiferayIdeaUI.LIFERAY_ICON);
+	}
+
+	@Override
+	public boolean isVisible(AnActionEvent e) {
+		if (getVirtualFile(e) != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getTaskName() {
+		return "initBundle";
+	}
+
+	@Override
+	protected String getWorkDirectory(AnActionEvent e) {
+		Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+
+		return project.getBasePath();
+	}
+
+}
