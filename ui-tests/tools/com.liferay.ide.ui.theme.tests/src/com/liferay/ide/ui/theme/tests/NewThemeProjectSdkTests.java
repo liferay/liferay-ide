35f1ee5fc1a3a95de012f0b548bd8381eafc9878
Terry Jia <terry.jia@liferay.com>
2017-11-16 18:32:50 +0800

    add more tests file

---
+++ ui-tests/tools/com.liferay.ide.ui.theme.tests/src/com/liferay/ide/ui/theme/tests/NewThemeProjectSdkTests.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.ui.theme.tests;
+
+import com.liferay.ide.ui.liferay.SwtbotBase;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Terry Jia
+ */
+public class NewThemeProjectSdkTests extends SwtbotBase {
+
+	@AfterClass
+	public static void cleanPluginsSdk() {
+	}
+
+	@BeforeClass
+	public static void createPluginsSdk() throws IOException {
+	}
+
+	@Test
+	@Ignore("wait for IDE-3568")
+	public void createTheme() {
+	}
+
+}
