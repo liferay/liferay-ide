46a943257db1eeb35f99c0850e225d121d139620
Ying Xu <ying.xu@liferay.com>
2017-12-28 16:55:29 +0800

    IDE-3594 add new module project wizard liferay workspace maven tests

--- ui-tests/tools/com.liferay.ide.ui.module.tests/src/com/liferay/ide/ui/module/tests/NewModuleProjectWizardLiferayWorkspaceMavenTests.java
+++ ui-tests/tools/com.liferay.ide.ui.module.tests/src/com/liferay/ide/ui/module/tests/NewModuleProjectWizardLiferayWorkspaceMavenTests.java
@@ -16,15 +16,353 @@ package com.liferay.ide.ui.module.tests;
 
 import com.liferay.ide.ui.liferay.base.LiferayWorkspaceTomcatMavenBase;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * @author Terry Jia
+ * @author Ying Xu
  */
 public class NewModuleProjectWizardLiferayWorkspaceMavenTests extends LiferayWorkspaceTomcatMavenBase {
 
 	@Test
 	public void createActivator() {
+		String projectName = "test-activator-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, ACTIVATOR);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createApi() {
+		String projectName = "test-api-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, API);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createContentTargetingReport() {
+		String projectName = "test-content-targeting-report-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, CONTENT_TARGETING_REPORT);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createContentTargetingRule() {
+		String projectName = "test-content-targeting-rule-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, CONTENT_TARGETING_RULE);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createContentTargetingTrackingAction() {
+		String projectName = "test-content-targeting-tracking-action-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, CONTENT_TARGETING_TRACKING_ACTION);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createControlMenuEntry() {
+		String projectName = "test-control-menu-entry-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, CONTROL_MENU_ENTRY);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createFormField() {
+		String projectName = "test-form-field-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, FORM_FIELD);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createPanelApp() {
+		String projectName = "test-panel-app-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, PANEL_APP);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createPortletConfigurationIcon() {
+		String projectName = "test-portlet-configuration-icon-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, PORTLET_CONFIGURATION_ICON);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createPortletProvider() {
+		String projectName = "test-portlet-provider-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, PORTLET_PROVIDER);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createPortletToolbarContributor() {
+		String projectName = "test-portlet-toolbar-contributor-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, PORTLET_TOOLBAR_CONTRIBUTOR);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createRest() {
+		String projectName = "test-rest-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, REST);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createService() {
+		String projectName = "test-service-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, SERVICE);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createServiceWrapper() {
+		String projectName = "test-service-wrapper-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, SERVICE_WRAPPER);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createSimulationPanelEntry() {
+		String projectName = "test-simulation-panel-entry-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, SIMULATION_PANEL_ENTRY);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createTemplateContextContributor() {
+		String projectName = "test-template-context-contributor-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, TEMPLATE_CONTEXT_CONCONTRIBUTOR);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createThemeContributor() {
+		String projectName = "test-theme-contributor-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, THEME_CONTRIBUTOR);
+
+		wizardAction.finish();
+
+		String[] projectNames =
+			{getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-modules (in modules)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createWarHook() {
+		String projectName = "test-war-hook-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, WAR_HOOK);
+
+		wizardAction.finish();
+
+		String[] projectNames = {getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-wars (in wars)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+	}
+
+	@Test
+	public void createWarMvcPortlet() {
+		String projectName = "test-war-mvc-portlet-in-lrws-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, WAR_MVC_PORTLET);
+
+		wizardAction.finish();
+
+		String[] projectNames = {getLiferayWorkspaceName(), getLiferayWorkspaceName() + "-wars (in wars)", projectName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
 	}
 
 }
