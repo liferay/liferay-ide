8491f05a62e9399ac1390f525fcf45d2abb6c14e
Terry Jia <terry.jia@liferay.com>
2017-12-31 23:33:27 +0800

    add more deployment tests

---
+++ ui-tests/tools/com.liferay.ide.ui.module.tests/src/com/liferay/ide/ui/module/tests/DeployModuleLiferayWorkspaceGradleTomcatTests.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.ui.module.tests;
+
+import com.liferay.ide.ui.liferay.SwtbotBase;
+import com.liferay.ide.ui.liferay.base.LiferayWorkspaceGradleSupport;
+import com.liferay.ide.ui.liferay.base.TomcatRunningSupport;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Terry Jia
+ */
+public class DeployModuleLiferayWorkspaceGradleTomcatTests extends SwtbotBase {
+
+	@ClassRule
+	public static LiferayWorkspaceGradleSupport liferayWorkspace = new LiferayWorkspaceGradleSupport(bot);
+
+	@ClassRule
+	public static TomcatRunningSupport tomcat = new TomcatRunningSupport(bot);
+
+	@Test
+	public void deployActivator() {
+		String projectName = "deploy-activator-gradle";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareGradle(projectName, ACTIVATOR);
+
+		wizardAction.finish();
+
+		// need to use job instead
+
+		ide.sleep(5000);
+
+		String[] projectNames = {liferayWorkspace.getLiferayWorkspaceName(), "modules", projectName};
+
+		viewAction.servers.openAddAndRemoveDialog(tomcat.getServerStartedLabel());
+
+		dialogAction.addAndRemove.addModule(projectName);
+
+		dialogAction.confirm(FINISH);
+
+		jobAction.waitForConsoleContent(
+			tomcat.getServerName(), "STARTED " + projectName.replace('-', '.') + "_", 20 * 1000);
+
+		viewAction.project.closeAndDelete(projectNames);
+	}
+
+}
---
+++ ui-tests/tools/com.liferay.ide.ui.module.tests/src/com/liferay/ide/ui/module/tests/DeployModuleLiferayWorkspaceMavenTomcatTests.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.ui.module.tests;
+
+import com.liferay.ide.ui.liferay.SwtbotBase;
+import com.liferay.ide.ui.liferay.base.LiferayWorkspaceMavenSupport;
+import com.liferay.ide.ui.liferay.base.TomcatRunningSupport;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+
+/**
+ * @author Terry Jia
+ */
+public class DeployModuleLiferayWorkspaceMavenTomcatTests extends SwtbotBase {
+
+	@ClassRule
+	public static LiferayWorkspaceMavenSupport liferayWorkspace = new LiferayWorkspaceMavenSupport(bot);
+
+	@ClassRule
+	public static TomcatRunningSupport tomcat = new TomcatRunningSupport(bot);
+
+	@Test
+	public void deployActivator() {
+		String projectName = "deploy-activator-maven";
+
+		wizardAction.openNewLiferayModuleWizard();
+
+		wizardAction.newModule.prepareMaven(projectName, ACTIVATOR);
+
+		wizardAction.finish();
+
+		// need to use job instead
+
+		ide.sleep(5000);
+
+		String[] projectNames =
+			{liferayWorkspace.getLiferayWorkspaceName(), liferayWorkspace.getModulesDirName(), projectName};
+
+		viewAction.servers.openAddAndRemoveDialog(tomcat.getServerStartedLabel());
+
+		dialogAction.addAndRemove.addModule(projectName);
+
+		dialogAction.confirm(FINISH);
+
+		jobAction.waitForConsoleContent(
+			tomcat.getServerName(), "STARTED " + projectName.replace('-', '.') + "_", 20 * 1000);
+
+		viewAction.project.closeAndDelete(projectNames);
+	}
+
+}
