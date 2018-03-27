9cfe747ce3d7fa59636d249db66ab4cb288ca9b5
Terry Jia <terry.jia@liferay.com>
2018-03-27 18:21:05 +0800

    Reconstruct the ui tests for 70, 71 and dxp

---
+++ ui-tests/base/com.liferay.ide.ui.fragment.base/src/com/liferay/ide/ui/fragment/base/NewFragmentWizardGradleBase.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.ui.fragment.base;
+
+import com.liferay.ide.ui.liferay.SwtbotBase;
+import com.liferay.ide.ui.liferay.support.project.ProjectSupport;
+
+import org.junit.Assert;
+import org.junit.Rule;
+
+/**
+ * @author Vicky Wang
+ * @author Sunny Shi
+ * @author Lily Li
+ * @author Ying Xu
+ */
+public class NewFragmentWizardGradleBase extends SwtbotBase {
+
+	public void createFragmentWithJsp() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.announcements.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("META-INF/resources/init.jsp");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.closeAndDelete(project.getName());
+	}
+
+	public void createFragmentWithJspf() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.document.library.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("META-INF/resources/document_library/cast_result.jspf");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.closeAndDelete(project.getName());
+	}
+
+	public void createFragmentWithoutFiles() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.hello.world.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		Assert.assertFalse(dialogAction.getConfirmBtn().isEnabled());
+
+		dialogAction.cancel();
+
+		wizardAction.finish();
+
+		viewAction.project.closeAndDelete(project.getName());
+	}
+
+	public void createFragmentWithPortletProperites() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.blogs.web");
+
+		dialogAction.confirm();
+
+		String[] files = {
+			"META-INF/resources/blogs_admin/configuration.jsp", "META-INF/resources/blogs_aggregator/init.jsp",
+			"META-INF/resources/blogs/asset/abstract.jsp", "META-INF/resources/blogs/edit_entry.jsp",
+			"portlet.properties"
+		};
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems(files);
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.closeAndDelete(project.getName());
+	}
+
+	public void createFragmentWithResourceAction() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.bookmarks.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("resource-actions/default.xml");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.closeAndDelete(project.getName());
+	}
+
+	public void createFragmentWithWholeFiles() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.asset.tags.navigation.web");
+
+		dialogAction.confirm();
+
+		String[] files = {
+			"META-INF/resources/configuration.jsp", "META-INF/resources/init-ext.jsp", "META-INF/resources/init.jsp",
+			"META-INF/resources/view.jsp", "portlet.properties", "resource-actions/default.xml"
+		};
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems(files);
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		Assert.assertFalse(dialogAction.getConfirmBtn().isEnabled());
+
+		dialogAction.cancel();
+
+		wizardAction.newFragmentInfo.selectFile("META-INF/resources/configuration.jsp");
+
+		wizardAction.newFragmentInfo.deleteFile();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("META-INF/resources/configuration.jsp");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.closeAndDelete(project.getName());
+	}
+
+	@Rule
+	public ProjectSupport project = new ProjectSupport(bot);
+
+}
---
+++ ui-tests/base/com.liferay.ide.ui.fragment.base/src/com/liferay/ide/ui/fragment/base/NewFragmentWizardLiferayWorkspaceGradleBase.java
@@ -0,0 +1,292 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.ui.fragment.base;
+
+import com.liferay.ide.ui.liferay.SwtbotBase;
+import com.liferay.ide.ui.liferay.support.project.ProjectSupport;
+import com.liferay.ide.ui.liferay.support.workspace.LiferayWorkspaceGradleSupport;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+
+/**
+ * @author Lily Li
+ */
+public class NewFragmentWizardLiferayWorkspaceGradleBase extends SwtbotBase {
+
+	@ClassRule
+	public static LiferayWorkspaceGradleSupport liferayWorkspace = new LiferayWorkspaceGradleSupport(bot);
+
+	public void createFragmentChangeModulesDir() {
+		viewAction.project.openFile(liferayWorkspace.getName(), "gradle.properties");
+
+		StringBuffer sb = new StringBuffer();
+
+		String newModulesFolderName = "modulesTest";
+
+		sb.append("liferay.workspace.modules.dir");
+		sb.append("=");
+		sb.append(newModulesFolderName);
+
+		editorAction.setText(sb.toString());
+
+		editorAction.save();
+
+		editorAction.close();
+
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.site.navigation.site.map.web");
+
+		dialogAction.confirm();
+
+		String[] files = {
+			"META-INF/resources/configuration.jsp", "META-INF/resources/init-ext.jsp", "META-INF/resources/init.jsp",
+			"META-INF/resources/view.jsp", "portlet.properties", "resource-actions/default.xml"
+		};
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems(files);
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.refreshGradleProject(liferayWorkspace.getName());
+
+		String[] projectNames = {liferayWorkspace.getName(), newModulesFolderName, project.getName()};
+
+		String[] newModulesFolderNames = {liferayWorkspace.getName(), newModulesFolderName};
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(projectNames));
+
+		viewAction.project.closeAndDeleteFromDisk(projectNames);
+
+		viewAction.project.closeAndDeleteFromDisk(newModulesFolderNames);
+
+		viewAction.project.openFile(liferayWorkspace.getName(), "gradle.properties");
+
+		sb.delete(0, sb.length());
+		sb.append("liferay.workspace.modules.dir=modules");
+
+		editorAction.setText(sb.toString());
+
+		editorAction.save();
+
+		editorAction.close();
+	}
+
+	public void createFragmentWithJsp() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.layout.admin.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("META-INF/resources/add_layout.jsp");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.refreshGradleProject(liferayWorkspace.getName());
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(liferayWorkspace.getModuleFiles(project.getName())));
+
+		viewAction.project.closeAndDeleteFromDisk(liferayWorkspace.getModuleFiles(project.getName()));
+	}
+
+	public void createFragmentWithJspf() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.site.memberships.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("META-INF/resources/role_columns.jspf");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.refreshGradleProject(liferayWorkspace.getName());
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(liferayWorkspace.getModuleFiles(project.getName())));
+
+		viewAction.project.closeAndDeleteFromDisk(liferayWorkspace.getModuleFiles(project.getName()));
+	}
+
+	public void createFragmentWithoutFiles() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.asset.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		Assert.assertFalse(dialogAction.getConfirmBtn().isEnabled());
+
+		dialogAction.cancel();
+
+		wizardAction.finish();
+
+		viewAction.project.refreshGradleProject(liferayWorkspace.getName());
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(liferayWorkspace.getModuleFiles(project.getName())));
+
+		viewAction.project.closeAndDeleteFromDisk(liferayWorkspace.getModuleFiles(project.getName()));
+	}
+
+	public void createFragmentWithPortletProperites() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.dynamic.data.mapping.web");
+
+		dialogAction.confirm();
+
+		String[] files = {
+			"META-INF/resources/template_add_buttons.jsp", "META-INF/resources/error.jsp",
+			"META-INF/resources/init.jsp", "portlet.properties"
+		};
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems(files);
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		viewAction.project.refreshGradleProject(liferayWorkspace.getName());
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(liferayWorkspace.getModuleFiles(project.getName())));
+
+		viewAction.project.closeAndDeleteFromDisk(liferayWorkspace.getModuleFiles(project.getName()));
+	}
+
+	public void createFragmentWithResourceAction() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.login.web");
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("resource-actions/default.xml");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(liferayWorkspace.getModuleFiles(project.getName())));
+
+		viewAction.project.closeAndDeleteFromDisk(liferayWorkspace.getModuleFiles(project.getName()));
+	}
+
+	public void createFragmentWithWholeFiles() {
+		wizardAction.openNewFragmentWizard();
+
+		wizardAction.newFragment.prepareGradle(project.getName());
+
+		wizardAction.next();
+
+		wizardAction.newFragmentInfo.openBrowseOsgiBundleDialog();
+
+		dialogAction.prepareText("com.liferay.xsl.content.web");
+
+		dialogAction.confirm();
+
+		String[] files = {
+			"META-INF/resources/configuration.jsp", "META-INF/resources/init-ext.jsp", "META-INF/resources/init.jsp",
+			"META-INF/resources/view.jsp", "portlet.properties", "resource-actions/default.xml"
+		};
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems(files);
+
+		dialogAction.confirm();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		Assert.assertFalse(dialogAction.getConfirmBtn().isEnabled());
+
+		dialogAction.cancel();
+
+		wizardAction.newFragmentInfo.selectFile("META-INF/resources/configuration.jsp");
+
+		wizardAction.newFragmentInfo.deleteFile();
+
+		wizardAction.newFragmentInfo.openAddOverrideFilesDialog();
+
+		dialogAction.selectItems("META-INF/resources/configuration.jsp");
+
+		dialogAction.confirm();
+
+		wizardAction.finish();
+
+		Assert.assertTrue(viewAction.project.visibleFileTry(liferayWorkspace.getModuleFiles(project.getName())));
+
+		viewAction.project.closeAndDeleteFromDisk(liferayWorkspace.getModuleFiles(project.getName()));
+	}
+
+	@Rule
+	public ProjectSupport project = new ProjectSupport(bot);
+
+}
