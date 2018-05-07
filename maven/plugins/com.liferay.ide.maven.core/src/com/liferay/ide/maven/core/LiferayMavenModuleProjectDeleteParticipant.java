6b11beffa60afb01769e4e0181442ca3f813831f
Simon Jiang <simon.jiang@liferay.com>
2018-05-07 16:02:03 +0800

    Camork-IDE-3987 use maven native methods instead of directly updating pom

--- maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/LiferayMavenModuleProjectDeleteParticipant.java
+++ maven/plugins/com.liferay.ide.maven.core/src/com/liferay/ide/maven/core/LiferayMavenModuleProjectDeleteParticipant.java
@@ -14,30 +14,33 @@
 
 package com.liferay.ide.maven.core;
 
+import com.liferay.ide.core.util.CoreUtil;
+import com.liferay.ide.core.util.FileUtil;
+
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.project.MavenProject;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.DeleteParticipant;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
 
 /**
  * @author Charles Wu
+ * @author Simon Jiang
  */
 public class LiferayMavenModuleProjectDeleteParticipant extends DeleteParticipant {
 
@@ -55,29 +58,30 @@ public class LiferayMavenModuleProjectDeleteParticipant extends DeleteParticipan
 	public Change createChange(IProgressMonitor pm) throws CoreException, OperationCanceledException {
 		CompositeChange change = new CompositeChange(getName());
 
-		IPath moduleLocation = _moduleProject.getLocation();
-
-		File pomFile = moduleLocation.removeLastSegments(1).append("pom.xml").toFile();
-
-		change.add(new RemoveModuleFromParentChange(pomFile));
+		change.add(new RemoveModuleFromParentChange(_parentMavenProject));
 
 		return change;
 	}
 
 	@Override
 	public String getName() {
-		return _REMOVE_MAVEN_MODULE_FROM_WORKSPACE;
+		return "Remove module from parent maven project " + _parentMavenProject.getName();
 	}
 
 	public class RemoveModuleFromParentChange extends Change {
 
-		public RemoveModuleFromParentChange(File pomFile) {
-			_pomFile = pomFile;
+		public RemoveModuleFromParentChange(MavenProject mavenProject) {
+			_mavenProject = mavenProject;
+		}
+
+		@Override
+		public Object getModifiedElement() {
+			return _mavenProject;
 		}
 
 		@Override
 		public String getName() {
-			return _REMOVE_MAVEN_MODULE_FROM_WORKSPACE;
+			return "Remove module project " + _moduleProject.getName();
 		}
 
 		@Override
@@ -91,40 +95,36 @@ public class LiferayMavenModuleProjectDeleteParticipant extends DeleteParticipan
 
 		@Override
 		public Change perform(IProgressMonitor pm) throws CoreException {
-			MavenXpp3Reader mavenReader = new MavenXpp3Reader();
+			String parentProjectName = _mavenProject.getName();
 
-			try (FileReader reader = new FileReader(_pomFile)) {
-				Model model = mavenReader.read(reader);
+			IProject parentProject = CoreUtil.getProject(parentProjectName);
 
-				List<String> modules = new ArrayList<>();
+			if (FileUtil.notExists(parentProject)) {
+				return null;
+			}
 
-				for (String name : model.getModules()) {
-					if (_moduleProject.getName().equals(name)) {
-						continue;
-					}
+			Model parentModel = _mavenProject.getOriginalModel();
 
-					modules.add(name);
-				}
+			parentModel.removeModule(_moduleProject.getName());
 
-				model.setModules(modules);
+			File pomFile = _mavenProject.getFile();
 
-				try (FileWriter fileWriter = new FileWriter(_pomFile)) {
-					MavenXpp3Writer mavenWriter = new MavenXpp3Writer();
+			if (FileUtil.notExists(pomFile)) {
+				return null;
+			}
 
-					mavenWriter.write(fileWriter, model);
-				}
+			try (FileWriter fileWriter = new FileWriter(pomFile)) {
+				MavenXpp3Writer mavenWriter = new MavenXpp3Writer();
+
+				mavenWriter.write(fileWriter, parentModel);
 			}
 			catch (Exception e) {
 			}
 
-			return null;
+			return this;
 		}
 
-		@Override public Object getModifiedElement() {
-			return _pomFile;
-		}
-
-		private File _pomFile;
+		private MavenProject _mavenProject;
 
 	}
 
@@ -136,31 +136,32 @@ public class LiferayMavenModuleProjectDeleteParticipant extends DeleteParticipan
 
 		_moduleProject = (IProject)element;
 
-		IPath moduleLocation = _moduleProject.getLocation();
+		IMavenProjectFacade mavenFacade = MavenUtil.getProjectFacade((IProject)element, new NullProgressMonitor());
 
-		File pomFile = moduleLocation.removeLastSegments(1).append("pom.xml").toFile();
+		try {
+			MavenProject selectedMavenProject = mavenFacade.getMavenProject(new NullProgressMonitor());
 
-		MavenXpp3Reader mavenReader = new MavenXpp3Reader();
-
-		try (FileReader reader = new FileReader(pomFile)) {
-			Model model = mavenReader.read(reader);
-
-			for (String name : model.getModules()) {
-				if (_moduleProject.getName().equals(name)) {
-					return true;
-				}
+			if (!selectedMavenProject.hasParent()) {
+				return false;
 			}
 
+			_parentMavenProject = selectedMavenProject.getParent();
+
+			if (_parentMavenProject == null) {
+				return false;
+			}
+
+			List<String> modules = _parentMavenProject.getModules();
+
+			return modules.contains(_moduleProject.getName());
 		}
-		catch (Exception e) {
-			return false;
+		catch (CoreException ce) {
 		}
 
 		return false;
 	}
 
-	private static final String _REMOVE_MAVEN_MODULE_FROM_WORKSPACE = "Remove module from parent maven pom.xml";
-
 	private IProject _moduleProject;
+	private MavenProject _parentMavenProject;
 
 }
