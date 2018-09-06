82decc393ac24bd77ea9911373e86fd0f7a01880
Ashley Yuan <yanan.yuan@liferay.com>
2018-09-06 11:20:39 +0800

    update ide version to 3.3.1

--- build/scripts/src/UpdateVersions.groovy
+++ build/scripts/src/UpdateVersions.groovy
@@ -1,5 +1,5 @@
-def oldVersion = "3.2.0"
-def newVersion = "3.3.0"
+def oldVersion = "3.3.0"
+def newVersion = "3.3.1"
 
 def oldQualVersion = "${oldVersion}.qualifier"
 def newQualVersion = "${newVersion}.qualifier"
