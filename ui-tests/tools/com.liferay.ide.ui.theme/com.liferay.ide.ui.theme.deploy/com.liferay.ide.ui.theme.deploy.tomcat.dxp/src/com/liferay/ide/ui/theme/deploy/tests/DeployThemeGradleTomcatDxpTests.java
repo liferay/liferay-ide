faea281e3f335f7ac976236fca126fc6e4b88a1e
Lily Li <lily.li@liferay.com>
2018-10-11 15:34:02 +0800

    IDE-4307 remove ignore for theme deploy caused by IDE-4188

--- ui-tests/tools/com.liferay.ide.ui.theme/com.liferay.ide.ui.theme.deploy/com.liferay.ide.ui.theme.deploy.tomcat.dxp/src/com/liferay/ide/ui/theme/deploy/tests/DeployThemeGradleTomcatDxpTests.java
+++ ui-tests/tools/com.liferay.ide.ui.theme/com.liferay.ide.ui.theme.deploy/com.liferay.ide.ui.theme.deploy.tomcat.dxp/src/com/liferay/ide/ui/theme/deploy/tests/DeployThemeGradleTomcatDxpTests.java
@@ -20,7 +20,6 @@ import com.liferay.ide.ui.liferay.util.RuleUtil;
 import com.liferay.ide.ui.theme.deploy.base.DeployThemeGradleTomcat7xBase;
 
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 
@@ -40,7 +39,6 @@ public class DeployThemeGradleTomcatDxpTests extends DeployThemeGradleTomcat7xBa
 		return server;
 	}
 
-	@Ignore("ignore as IDE-4188")
 	@Test
 	public void deployTheme() {
 		super.deployTheme();
