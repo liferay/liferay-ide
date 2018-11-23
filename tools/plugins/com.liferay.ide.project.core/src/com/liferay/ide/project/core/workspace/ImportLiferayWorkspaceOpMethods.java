2772de9210970c529bfc2a0d52d23462f658f724
Simon Jiang <simon.jiang@liferay.com>
2018-11-23 09:36:32 +0800

    IDE-4401 packaging add portal runtime into job for import workspace

--- tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/workspace/ImportLiferayWorkspaceOpMethods.java
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/workspace/ImportLiferayWorkspaceOpMethods.java
@@ -14,10 +14,15 @@
 
 package com.liferay.ide.project.core.workspace;
 
+import com.liferay.ide.core.LiferayCore;
+import com.liferay.ide.project.core.jobs.JobUtil;
 import com.liferay.ide.project.core.util.LiferayWorkspaceUtil;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.sapphire.Value;
 import org.eclipse.sapphire.modeling.Path;
 import org.eclipse.sapphire.modeling.ProgressMonitor;
@@ -29,6 +34,7 @@ import org.eclipse.sapphire.platform.StatusBridge;
 /**
  * @author Andy Wu
  * @author Terry Jia
+ * @author Simon Jiang
  */
 public class ImportLiferayWorkspaceOpMethods {
 
@@ -85,7 +91,32 @@ public class ImportLiferayWorkspaceOpMethods {
 			}
 
 			if (!initBundle && hasBundlesDir) {
-				LiferayWorkspaceUtil.addPortalRuntime(serverName);
+				Job addPortalRuntimeJob = new Job("Add Liferay Portal Runtime Job") {
+
+					@Override
+					public boolean belongsTo(Object family) {
+						return family.equals(LiferayCore.LIFERAY_JOB_FAMILY);
+					}
+
+					protected IStatus run(IProgressMonitor monitor) {
+						LiferayWorkspaceUtil.addPortalRuntime(serverName);
+
+						return StatusBridge.create(Status.createOkStatus());
+					}
+
+				};
+
+				addPortalRuntimeJob.addJobChangeListener(
+					new JobChangeAdapter() {
+
+						@Override
+						public void aboutToRun(IJobChangeEvent event) {
+							JobUtil.waitForLiferayProjectJob();
+						}
+
+					});
+
+				addPortalRuntimeJob.schedule();
 			}
 		}
 		catch (Exception e) {
