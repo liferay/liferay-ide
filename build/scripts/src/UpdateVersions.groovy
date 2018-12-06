d71ca5c8e261e0e552abf912cb1205690e67836e
Ashley Yuan <yanan.yuan@liferay.com>
2018-12-06 13:06:09 +0800

    IDE-4428 update ide version to 3.5.0

--- build/scripts/src/UpdateVersions.groovy
+++ build/scripts/src/UpdateVersions.groovy
@@ -1,5 +1,5 @@
-def oldVersion = "3.3.1"
-def newVersion = "3.4.0"
+def oldVersion = "3.4.0"
+def newVersion = "3.5.0"
 
 def oldQualVersion = "${oldVersion}.qualifier"
 def newQualVersion = "${newVersion}.qualifier"
