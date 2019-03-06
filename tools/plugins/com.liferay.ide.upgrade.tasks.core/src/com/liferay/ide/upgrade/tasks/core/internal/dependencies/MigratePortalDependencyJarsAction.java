c0e3825b99b9a87a4ddfb0e3ca68e92e631d9705
Terry Jia <terry.jia@liferay.com>
2019-03-06 10:40:12 +0800

    IDE-3972 add some actions for migrating sdk dependencies

---
+++ tools/plugins/com.liferay.ide.upgrade.tasks.core/src/com/liferay/ide/upgrade/tasks/core/internal/dependencies/MigratePortalDependencyJarsAction.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.upgrade.tasks.core.internal.dependencies;
+
+import com.liferay.ide.upgrade.plan.core.BaseUpgradeTaskStepAction;
+import com.liferay.ide.upgrade.plan.core.UpgradeTaskStepAction;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Terry Jia
+ */
+@Component(
+	property = {
+		"id=" + MigratePortalDependencyJarsActionKeys.ID,
+		"description=" + MigratePortalDependencyJarsActionKeys.DESCRIPTION, "order=1",
+		"stepId=" + UpdatePluginsSDKDependenciesStepKeys.ID, "title=" + MigratePortalDependencyJarsActionKeys.TITLE
+	},
+	scope = ServiceScope.PROTOTYPE, service = UpgradeTaskStepAction.class
+)
+public class MigratePortalDependencyJarsAction extends BaseUpgradeTaskStepAction {
+
+	@Override
+	public IStatus perform(IProgressMonitor progressMonitor) {
+		return null;
+	}
+
+}
