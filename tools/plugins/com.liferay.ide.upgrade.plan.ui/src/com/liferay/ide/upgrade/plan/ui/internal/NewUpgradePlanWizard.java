69a08a7c4be01f03c69c19b3619812228900278c
Simon Jiang <simon.jiang@liferay.com>
2019-03-06 10:46:55 +0800

    IDE-3972 disable planner wizard error message for first time

--- tools/plugins/com.liferay.ide.upgrade.plan.ui/src/com/liferay/ide/upgrade/plan/ui/internal/NewUpgradePlanWizard.java
+++ tools/plugins/com.liferay.ide.upgrade.plan.ui/src/com/liferay/ide/upgrade/plan/ui/internal/NewUpgradePlanWizard.java
@@ -17,18 +17,37 @@ package com.liferay.ide.upgrade.plan.ui.internal;
 import com.liferay.ide.upgrade.plan.core.NewUpgradePlanOp;
 
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.sapphire.ui.def.DefinitionLoader;
 import org.eclipse.sapphire.ui.forms.swt.SapphireWizard;
+import org.eclipse.sapphire.ui.forms.swt.SapphireWizardPage;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
 
 /**
  * @author Gregory Amerson
  * @author Terry Jia
+ * @author Simon Jiang
  */
 public class NewUpgradePlanWizard extends SapphireWizard<NewUpgradePlanOp> {
 
 	public NewUpgradePlanWizard() {
 		super(NewUpgradePlanOp.TYPE, DefinitionLoader.sdef(NewUpgradePlanWizard.class).wizard());
+	}
+
+	@Override
+	public IWizardPage[] getPages() {
+		final IWizardPage[] wizardPages = super.getPages();
+
+		if (!_firstErrorMessageRemoved && (wizardPages != null)) {
+			SapphireWizardPage wizardPage = (SapphireWizardPage)wizardPages[0];
+
+			wizardPage.setMessage(
+				"Create and start a new Upgrade Plan. Any existing plan will be replaced.", SapphireWizardPage.NONE);
+
+			_firstErrorMessageRemoved = true;
+		}
+
+		return wizardPages;
 	}
 
 	@Override
@@ -50,5 +69,7 @@ public class NewUpgradePlanWizard extends SapphireWizard<NewUpgradePlanOp> {
 
 		BasicNewProjectResourceWizard.updatePerspective(element);
 	}
+
+	private boolean _firstErrorMessageRemoved = false;
 
 }
