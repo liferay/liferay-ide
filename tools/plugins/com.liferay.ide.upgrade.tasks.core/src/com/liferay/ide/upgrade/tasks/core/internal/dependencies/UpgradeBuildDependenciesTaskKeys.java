6943b266c5941d8c0833de18e82ef61e7a69d912
Gregory Amerson <gregory.amerson@liferay.com>
2019-03-06 10:38:59 -0600

    IDE-3972 update formating

--- tools/plugins/com.liferay.ide.upgrade.tasks.core/src/com/liferay/ide/upgrade/tasks/core/internal/dependencies/UpgradeBuildDependenciesTaskKeys.java
+++ tools/plugins/com.liferay.ide.upgrade.tasks.core/src/com/liferay/ide/upgrade/tasks/core/internal/dependencies/UpgradeBuildDependenciesTaskKeys.java
@@ -21,9 +21,9 @@ public class UpgradeBuildDependenciesTaskKeys {
 
 	public static final String DESCRIPTION =
 		"Plugins rely on their dependencies' availability at compile time and run time.\n Some classes are listed as " +
-			"undefined classes or unresolved symbols in your legacy projects because they've been moved, renamed, or " +
-				"removed. As a part of modularization in Liferay Portal, many of these classes reside in new " +
-					"modules. To run your project, the container must be able to find all your plugins.";
+		"undefined classes or unresolved symbols in your legacy projects because they've been moved, renamed, or " +
+		"removed. As a part of modularization in Liferay Portal, many of these classes reside in new modules. To run " +
+		"your project, the container must be able to find all your plugins.";
 
 	public static final String ID = "upgrade_build_dependencies";
 
