faebfbd925c03bd5210f6276f7dfb3da54faf58e
Seiphon <871358932@qq.com>
2019-05-21 10:20:22 +0800

    author and copyright

--- tools/tests/com.liferay.ide.upgrade.problems.core.tests/src/DeprecatedInvokableServiceInterface/InvokableServiceInterfaceTest.java
+++ tools/tests/com.liferay.ide.upgrade.problems.core.tests/src/DeprecatedInvokableServiceInterface/InvokableServiceInterfaceTest.java
@@ -1,9 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package DeprecatedInvokableServiceInterface;
 
 import java.io.File;
 
 import com.liferay.ide.upgrade.problems.test.apichanges.APITestBase;
 
+/**
+ * @author Seiphon Wang
+ */
 public class InvokableServiceInterfaceTest extends APITestBase {
 
 	@Override
