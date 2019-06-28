8c0461e081da52e5975ee1e46dd76f2c934788e7
Ashley Yuan <yanan.yuan@liferay.com>
2019-06-28 17:46:19 +0800

    update to latest test dependencies

--- ui-tests/common/com.liferay.ide.ui.liferay/src/com/liferay/ide/ui/liferay/support/server/PureTomcatDxp70Support.java
+++ ui-tests/common/com.liferay.ide.ui.liferay/src/com/liferay/ide/ui/liferay/support/server/PureTomcatDxp70Support.java
@@ -30,7 +30,7 @@ public class PureTomcatDxp70Support extends ServerSupport {
 	}
 
 	public PureTomcatDxp70Support(SWTWorkbenchBot bot) {
-		super(bot, "tomcat", "7.0-sp9");
+		super(bot, "tomcat", "7.0-sp11");
 	}
 
 }
