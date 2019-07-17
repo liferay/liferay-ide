817050a385d80cab3a62eee8b72d8c37c15adfd4
Gregory Amerson <gregory.amerson@liferay.com>
2019-07-17 08:53:44 -0500

    IDE-3330 simplify and combine into one class

--- tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/LiferayServerLogFileStreamsProxy.java
+++ tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/LiferayServerLogFileStreamsProxy.java
@@ -14,6 +14,11 @@
 
 package com.liferay.ide.server.core.portal;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IStreamMonitor;
@@ -21,7 +26,7 @@ import org.eclipse.debug.core.model.IStreamMonitor;
 /**
  * @author Simon Jiang
  */
-public class LiferayServerLogFileStreamsProxy extends LiferayServerFileStreamsProxy {
+public class LiferayServerLogFileStreamsProxy {
 
 	public LiferayServerLogFileStreamsProxy(PortalRuntime runtime, ILaunch curLaunch) {
 		this(runtime, curLaunch, new LiferayServerOutputStreamMonitor(), new LiferayServerOutputStreamMonitor());
@@ -67,6 +72,156 @@ public class LiferayServerLogFileStreamsProxy extends LiferayServerFileStreamsPr
 		return _launch;
 	}
 
+	public IStreamMonitor getOutputStreamMonitor() {
+		return sysOut;
+	}
+
+	public boolean isTerminated() {
+		return _done;
+	}
+
+	public void terminate() {
+		if (_bufferOut != null) {
+			try {
+				_bufferOut.close();
+
+				_bufferOut = null;
+			}
+			catch (Exception e) {
+			}
+		}
+
+		_done = true;
+	}
+
+	public void write(String input) throws IOException {
+	}
+
+	protected void readToNow(BufferedReader br) throws IOException {
+		String s = "";
+
+		while (s != null) {
+			s = br.readLine();
+		}
+	}
+
+	protected final boolean shouldReloadFileReader(long originalFileSize, long newFileSize) {
+		boolean reloadFileReader = true;
+
+		if (originalFileSize <= newFileSize) {
+			reloadFileReader = false;
+		}
+
+		return reloadFileReader;
+	}
+
+	protected void startMonitoring() {
+		if (_streamThread != null) {
+			return;
+		}
+
+		_streamThread = new Thread("Liferay Portal Log Monitor Thread") {
+
+			public void run() {
+				boolean outInitialized = false;
+				boolean outFileEmpty = false;
+
+				while (!_done && !outInitialized) {
+					try {
+						_fpOut = (sysoutFile != null) ? new File(sysoutFile) : null;
+
+						if (!outInitialized) {
+							if (!_fpOut.exists()) {
+								outFileEmpty = true;
+							}
+							else {
+								outInitialized = true;
+							}
+						}
+					}
+					catch (Exception e) {
+					}
+
+					if (outInitialized) {
+						continue;
+					}
+
+					try {
+						sleep(200L);
+					}
+					catch (Exception e) {
+					}
+				}
+
+				try {
+					if (outInitialized) {
+						_bufferOut = new BufferedReader(new FileReader(_fpOut));
+
+						if (!outFileEmpty) {
+							readToNow(_bufferOut);
+						}
+					}
+				}
+				catch (Exception e) {
+				}
+
+				long originalFpOutSize = _fpOut.length();
+
+				while (!_done) {
+					try {
+						sleep(500L);
+					}
+					catch (Exception e) {
+					}
+
+					try {
+						String s = "";
+
+						while ((s != null) && !_done) {
+							long newFpOutSize = _fpOut.length();
+
+							if (shouldReloadFileReader(originalFpOutSize, newFpOutSize)) {
+								if (_bufferOut != null) {
+									_bufferOut.close();
+								}
+
+								_bufferOut = new BufferedReader(new FileReader(_fpOut));
+							}
+
+							originalFpOutSize = newFpOutSize;
+
+							if (_bufferOut != null) {
+								s = _bufferOut.readLine();
+
+								if (s != null) {
+									sysOut.append(s + "\n");
+								}
+							}
+						}
+					}
+					catch (Exception e) {
+					}
+				}
+
+				_streamThread = null;
+			}
+
+		};
+
+		_streamThread.setPriority(1);
+
+		_streamThread.setDaemon(true);
+
+		_streamThread.start();
+	}
+
+	protected LiferayServerOutputStreamMonitor sysOut;
+	protected String sysoutFile;
+
+	private BufferedReader _bufferOut = null;
+	private boolean _done = false;
+	private File _fpOut = null;
 	private ILaunch _launch;
+	private Thread _streamThread;
 
 }
