0c441748b0f3f96f6888f538c84fefecb8b405f9
Gregory Amerson <gregory.amerson@liferay.com>
2020-02-14 09:09:57 -0600

    IDE-4739 extract vars

--- tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/springmvcportlet/SpringMVCPortletConfigurationPresentationPart.java
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/springmvcportlet/SpringMVCPortletConfigurationPresentationPart.java
@@ -66,10 +66,12 @@ public class SpringMVCPortletConfigurationPresentationPart
 							if (_frameworkCombo.equals(e.getSource())) {
 								String value = _frameworkCombo.getText();
 
+								NewSpringMVCPortletProjectOp op = _op();
+
+								String version = get(op.getLiferayVersion());
+
 								if (value.equals(SpringMVCPortletProjectConstants.SPRING_FRAMEWORK[0])) {
 									_frameworkDependenciesCombo.removeAll();
-
-									String version = get(_op().getLiferayVersion());
 
 									if (version.equals(WorkspaceConstants.LIFERAY_VERSIONS[3])) {
 										_frameworkDependenciesCombo.setItems(
@@ -82,8 +84,6 @@ public class SpringMVCPortletConfigurationPresentationPart
 								}
 								else if (value.equals(SpringMVCPortletProjectConstants.SPRING_FRAMEWORK[1])) {
 									_frameworkDependenciesCombo.removeAll();
-
-									String version = get(_op().getLiferayVersion());
 
 									if (version.equals(WorkspaceConstants.LIFERAY_VERSIONS[1]) ||
 										version.equals(WorkspaceConstants.LIFERAY_VERSIONS[2])) {
@@ -99,7 +99,7 @@ public class SpringMVCPortletConfigurationPresentationPart
 
 								_frameworkDependenciesCombo.select(0);
 
-								_op().setFramework(value);
+								op.setFramework(value);
 							}
 						}
 
