c412594c99daa129c88bad14e442b003ee11f67a
Gregory Amerson <gregory.amerson@liferay.com>
2020-03-10 18:09:32 -0500

    use run-tests.sh script

---
+++ run-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function archiveTests {
+	tar cvzf surefire-reports.tar.gz $(find . -name '*surefire-reports*')
+}
+
+function checkError {
+	local retcode=$?
+
+	if [ $retcode -ne 0 ]; then
+		archiveTests
+
+		exit $retcode
+	fi
+}
+
+./mvnw --fail-at-end -e -Dmaven.repo.local=${GITHUB_WORKSPACE}/liferay-ide-m2-repository verify -Dliferay.bundles.dir="./tests-resources"
+
+checkError
