d48a5d74bf4bd2c04ac2946d8e78e6c4611db929
Simon Jiang <simon.jiang@liferay.com>
2020-06-23 18:06:50 +0800

    IDE-4821 Add a new wizard to prompt users to select liferay.workspace.product

---
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/workspace/ConfigureProductVersionDefaultValueService.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.project.core.workspace;
+
+import com.liferay.ide.core.util.SapphireContentAccessor;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.sapphire.DefaultValueService;
+import org.eclipse.sapphire.Event;
+import org.eclipse.sapphire.Listener;
+import org.eclipse.sapphire.PossibleValuesService;
+import org.eclipse.sapphire.Value;
+
+/**
+ * @author Simon Jiang
+ */
+public class ConfigureProductVersionDefaultValueService extends DefaultValueService implements SapphireContentAccessor {
+
+	public ConfigureProductVersionDefaultValueService() {
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+
+		PossibleValuesService possibleValuesService = _possibleValuesService();
+
+		possibleValuesService.detach(_listener);
+	}
+
+	@Override
+	protected String compute() {
+		return _defaultValue;
+	}
+
+	@Override
+	protected void initDefaultValueService() {
+		super.initDefaultValueService();
+
+		PossibleValuesService possibleValuesService = _possibleValuesService();
+
+		_listener = new Listener() {
+
+			@Override
+			public void handle(Event event) {
+				Set<String> values = possibleValuesService.values();
+
+				if (!values.isEmpty()) {
+					Iterator<String> iterator = values.iterator();
+
+					_defaultValue = iterator.next();
+
+					refresh();
+				}
+			}
+
+		};
+
+		possibleValuesService.attach(_listener);
+	}
+
+	private PossibleValuesService _possibleValuesService() {
+		ConfigureWorkspaceProductOp op = context(ConfigureWorkspaceProductOp.class);
+
+		Value<Object> property = op.property(ConfigureWorkspaceProductOp.PROP_PRODUCT_VERSION);
+
+		PossibleValuesService possibleValuesService = property.service(PossibleValuesService.class);
+
+		return possibleValuesService;
+	}
+
+	private String _defaultValue = null;
+	private Listener _listener;
+
+}
---
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/workspace/ConfigureWorkspaceProductOp.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.project.core.workspace;
+
+import org.eclipse.sapphire.ElementType;
+import org.eclipse.sapphire.ExecutableElement;
+import org.eclipse.sapphire.Type;
+import org.eclipse.sapphire.Value;
+import org.eclipse.sapphire.ValueProperty;
+import org.eclipse.sapphire.modeling.ProgressMonitor;
+import org.eclipse.sapphire.modeling.Status;
+import org.eclipse.sapphire.modeling.annotations.DefaultValue;
+import org.eclipse.sapphire.modeling.annotations.DelegateImplementation;
+import org.eclipse.sapphire.modeling.annotations.Label;
+import org.eclipse.sapphire.modeling.annotations.Required;
+import org.eclipse.sapphire.modeling.annotations.Service;
+
+/**
+ * @author Simon Jiang
+ */
+public interface ConfigureWorkspaceProductOp extends ExecutableElement {
+
+	public ElementType TYPE = new ElementType(ConfigureWorkspaceProductOp.class);
+
+	@DelegateImplementation(ConfigureWorkspaceProductOpMethods.class)
+	@Override
+	public Status execute(ProgressMonitor monitor);
+
+	public Value<String> getProductVersion();
+
+	public Value<Boolean> getShowAllVersionProduct();
+
+	public void setProductVersion(String name);
+
+	public void setShowAllVersionProduct(Boolean value);
+
+	@Label(standard = "Product Version")
+	@Required
+	@Service(impl = ConfigureProductVersionPossibleValuesService.class)
+	@Service(impl = ConfigureProductVersionDefaultValueService.class)
+	public ValueProperty PROP_PRODUCT_VERSION = new ValueProperty(TYPE, "ProductVersion");
+
+	@DefaultValue(text = "false")
+	@Label(standard = "Show All Version Product")
+	@Type(base = Boolean.class)
+	public ValueProperty PROP_SHOW_ALL_VERSION_PRODUCT = new ValueProperty(TYPE, "ShowAllVersionProduct");
+
+}
