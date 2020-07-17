6b40c0db22a73bfc509af47aded8b43065e5e11b
Seiphon Wang <99855397@liferay.com>
2020-07-17 11:29:06 +0800

    IDE-4838 7.3 Breaking change, remove AssetTagsSelectorTag

---
+++ tools/tests/com.liferay.ide.upgrade.problems.core.tests/projects/filetests/RemoveAssetTagsSelectorTagUnitTest.java
@@ -0,0 +1,7 @@
+import com.liferay.asset.taglib.servlet.taglib.soy.AssetTagsSelectorTag;
+
+public class RemoveAssetTagsSelectorTagUnitTest extends AssetTagsSelectorTag {
+	public static void main(String args[]) {
+		
+	}
+}
