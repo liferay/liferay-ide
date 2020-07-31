5f6b333f0800cca2a66cd17bf927bac1fcd20ef0
Gregory Amerson <gregory.amerson@liferay.com>
2020-07-30 23:57:28 -0500

    IDE-4613 skeleton files for generating offline upgrade plan docs

---
+++ build/liferay-docs-offline/generate_offline.sh
@@ -0,0 +1,7 @@
+# Download liferay-docs repo for a specific tag
+# https://github.com/gamerson/liferay-docs/archive/liferay-ide-offline-3.9-m2-20200730.zip
+# unzip the liferay-docs content
+# call the markdown-converter script to only convert this path
+# liferay-docs/en/developer/tutorials/articles/03-upgrading-code-to-liferay-7.2/
+#/bin/convert.sh /Users/greg/my\ repos/liferay-ide/build/liferay-docs-offline/liferay-docs-liferay-ide-offline-3.9-m2-20200730/en/developer/tutorials/articles/03-upgrading-code-to-liferay-7.2 upgrade_plan_export
+# upload new zip to bintray at this location:https://dl.bintray.com/gamerson/liferay-ide-files/zips/
