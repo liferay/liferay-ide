80c6d09d138f87e764a927f59c4ac2cef2d4f4d0
Gregory Amerson <gregory.amerson@liferay.com>
2020-08-20 11:57:15 -0500

    IDE-4209 when syncing back to IDE codebase, don't delete any files

--- tools/lugbot-rsync.sh
+++ tools/lugbot-rsync.sh
@@ -8,25 +8,31 @@ fi
 lugbotRepo=$1
 
 rsync \
-	-avu \
-	--delete \
-	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/internal/liferay70/" \ 
+	-av \
+	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/" \
+	"./plugins/com.liferay.ide.upgrade.problems.core/src/com/liferay/ide/upgrade/problems/core"
+
+rsync \
+	-av \
+	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/internal/" \
+	"./plugins/com.liferay.ide.upgrade.problems.core/src/com/liferay/ide/upgrade/problems/core/internal"
+
+rsync \
+	-av \
+	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/internal/liferay70/" \
 	"./plugins/com.liferay.ide.upgrade.problems.core/src/com/liferay/ide/upgrade/problems/core/internal/liferay70"
 
 rsync \
-	-avu \
-	--delete \
+	-av \
 	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/internal/liferay71/" \
 	"./plugins/com.liferay.ide.upgrade.problems.core/src/com/liferay/ide/upgrade/problems/core/internal/liferay71"
 
 rsync \
-	-avu \
-	--delete \
+	-av \
 	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/internal/liferay72/" \
 	"./plugins/com.liferay.ide.upgrade.problems.core/src/com/liferay/ide/upgrade/problems/core/internal/liferay72"
 
 rsync \
-	-avu \
-	--delete \
+	-av \
 	"$lugbotRepo/lugbot/com.liferay.code.upgrade.providers/src/com/liferay/ide/upgrade/problems/core/internal/liferay73/" \
 	"./plugins/com.liferay.ide.upgrade.problems.core/src/com/liferay/ide/upgrade/problems/core/internal/liferay73"
