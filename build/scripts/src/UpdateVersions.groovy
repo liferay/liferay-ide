d26b84087314b5639a864d7c16fe249626cbc04f
Ashley Yuan <yanan.yuan@liferay.com>
2021-09-03 15:44:21 +0800

    update version to 4.0.0

--- build/scripts/src/UpdateVersions.groovy
+++ build/scripts/src/UpdateVersions.groovy
@@ -1,5 +1,5 @@
-def oldVersion = "3.10.0"
-def newVersion = "3.9.4"
+def oldVersion = "3.9.4"
+def newVersion = "4.0.0"
 
 def oldQualVersion = "${oldVersion}.qualifier"
 def newQualVersion = "${newVersion}.qualifier"
