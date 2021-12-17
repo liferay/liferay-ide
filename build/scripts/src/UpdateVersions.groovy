9e2bb422e815885d48faf222c2e431010f3e9e35
Ashley Yuan <yanan.yuan@liferay.com>
2021-12-17 10:31:58 +0800

    IDE-5054 update version to 3.9.5

--- build/scripts/src/UpdateVersions.groovy
+++ build/scripts/src/UpdateVersions.groovy
@@ -1,5 +1,5 @@
-def oldVersion = "3.9.4"
-def newVersion = "4.0.0"
+def oldVersion = "4.0.0"
+def newVersion = "3.9.5"
 
 def oldQualVersion = "${oldVersion}.qualifier"
 def newQualVersion = "${newVersion}.qualifier"
