c9d5c2aa7741e2e6b155bc57ed8e2fe5d3163aae
drewbrokke <drew.brokke@liferay.com>
2024-08-22 18:10:36 -0500

    LPD-34604 build: sets new version in UpdateVersions.groovy script

--- build/scripts/src/UpdateVersions.groovy
+++ build/scripts/src/UpdateVersions.groovy
@@ -1,5 +1,5 @@
-def oldVersion = "3.10.0"
-def newVersion = "3.10.1"
+def oldVersion = "3.10.1"
+def newVersion = "3.10.2"
 
 def oldQualVersion = "${oldVersion}.qualifier"
 def newQualVersion = "${newVersion}.qualifier"
