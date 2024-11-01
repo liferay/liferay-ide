70118888195e696c9a9f8f4e965f4ce8188000ed
Drew Brokke <drew.brokke@liferay.com>
2024-11-01 16:31:05 -0500

    LPD-38770 com.liferay.ide.upgrade.plan.core: NewUpgradePlanOp: adds quarterly releases to the upgrade plan options

--- tools/plugins/com.liferay.ide.upgrade.plan.core/src/com/liferay/ide/upgrade/plan/core/NewUpgradePlanOp.java
+++ tools/plugins/com.liferay.ide.upgrade.plan.core/src/com/liferay/ide/upgrade/plan/core/NewUpgradePlanOp.java
@@ -14,15 +14,24 @@
 
 package com.liferay.ide.upgrade.plan.core;
 
+import com.liferay.ide.core.util.CoreUtil;
+import com.liferay.ide.core.util.ReleaseUtil;
 import com.liferay.ide.upgrade.plan.core.internal.NameValidationService;
 import com.liferay.ide.upgrade.plan.core.internal.NewUpgradePlanOpMethods;
 import com.liferay.ide.upgrade.plan.core.internal.OutlineDefaultValueService;
 import com.liferay.ide.upgrade.plan.core.internal.OutlinePossibleValuesService;
 import com.liferay.ide.upgrade.plan.core.internal.TargetVersionValidationService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.sapphire.DefaultValueService;
 import org.eclipse.sapphire.ElementType;
 import org.eclipse.sapphire.ExecutableElement;
-import org.eclipse.sapphire.PossibleValues;
+import org.eclipse.sapphire.PossibleValuesService;
 import org.eclipse.sapphire.Type;
 import org.eclipse.sapphire.Value;
 import org.eclipse.sapphire.ValueProperty;
@@ -72,16 +81,16 @@ public interface NewUpgradePlanOp extends ExecutableElement {
 
 	@DefaultValue(text = "6.2")
 	@Label(standard = "Current Liferay Version")
-	@PossibleValues(values = {"6.2", "7.0", "7.1", "7.2", "7.3"})
+	@Service(impl = CurrentVersionPossibleValuesService.class)
 	public ValueProperty PROP_CURRENT_VERSION = new ValueProperty(TYPE, "CurrentVersion");
 
 	@Required
 	@Service(impl = NameValidationService.class)
 	public ValueProperty PROP_NAME = new ValueProperty(TYPE, "Name");
 
-	@DefaultValue(text = "7.4")
 	@Label(standard = "Target Liferay Version")
-	@PossibleValues(values = {"7.0", "7.1", "7.2", "7.3", "7.4"})
+	@Service(impl = TargetVersionDefaultValueService.class)
+	@Service(impl = TargetVersionPossibleValuesService.class)
 	@Service(impl = TargetVersionValidationService.class)
 	public ValueProperty PROP_TARGET_VERSION = new ValueProperty(TYPE, "TargetVersion");
 
@@ -90,5 +99,55 @@ public interface NewUpgradePlanOp extends ExecutableElement {
 	@Service(impl = OutlinePossibleValuesService.class)
 	@Type(base = IUpgradePlanOutline.class)
 	public ValueProperty PROP_UPGRADE_PLAN_OUTLINE = new ValueProperty(TYPE, "UpgradePlanOutline");
+
+	public class CurrentVersionPossibleValuesService extends PossibleValuesService {
+
+		@Override
+		protected void compute(Set<String> values) {
+			List<String> productGroupVersions = new ArrayList<>(Arrays.asList(ReleaseUtil.getProductGroupVersions()));
+
+			productGroupVersions.add("6.2");
+
+			productGroupVersions.sort(new ProductGroupComparator());
+
+			values.addAll(productGroupVersions.subList(0, productGroupVersions.size() - 1));
+		}
+
+	}
+
+	public class ProductGroupComparator implements Comparator<String> {
+
+		@Override
+		public int compare(String o1, String o2) {
+			return CoreUtil.compareVersions(CoreUtil.parseVersion(o1), CoreUtil.parseVersion(o2));
+		}
+
+	}
+
+	public class TargetVersionDefaultValueService extends DefaultValueService {
+
+		@Override
+		protected String compute() {
+			List<String> productGroupVersions = Arrays.asList(ReleaseUtil.getProductGroupVersions());
+
+			productGroupVersions.sort(new ProductGroupComparator());
+
+			return productGroupVersions.get(productGroupVersions.size() - 1);
+		}
+
+	}
+
+	public class TargetVersionPossibleValuesService extends PossibleValuesService {
+
+		@Override
+		protected void compute(Set<String> values) {
+			List<String> productGroupVersions = Arrays.asList(ReleaseUtil.getProductGroupVersions());
+
+			productGroupVersions.sort(new ProductGroupComparator());
+
+			values.addAll(productGroupVersions);
+		}
+
+	}
 
 }
