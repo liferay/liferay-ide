d2b57122e6adb1e9d23b56b3996e27313adb838d
Drew Brokke <drew.brokke@liferay.com>
2026-03-20 16:39:16 -0500

    LPD-80322 adds SecureXMLFactoryUtil for XXE-safe XML parser creation
    Adds a shared utility that creates XML parser factories pre-configured
    to prevent XML External Entity (XXE) attacks. This will be used by
    AbstractDefaultHandler, PropertiesUtil, and FileUtil.
    
    https://find-sec-bugs.github.io/bugs.htm#XXE_SAXPARSER
    https://find-sec-bugs.github.io/bugs.htm#XXE_DOCUMENT
    https://find-sec-bugs.github.io/bugs.htm#XXE_DTD_TRANSFORM_FACTORY
    https://find-sec-bugs.github.io/bugs.htm#XXE_XSLT_TRANSFORM_FACTORY

---
+++ tools/plugins/com.liferay.ide.core/src/com/liferay/ide/core/util/SecureXMLFactoryUtil.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.core.util;
+
+import com.liferay.ide.core.LiferayCore;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.TransformerFactory;
+
+/**
+ * @author Drew Brokke
+ */
+public class SecureXMLFactoryUtil {
+
+	public static DocumentBuilderFactory newDocumentBuilderFactory() {
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+		try {
+			documentBuilderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false);
+			documentBuilderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			documentBuilderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			documentBuilderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+		}
+		catch (Exception exception) {
+			LiferayCore.logError("Unable to configure secure DocumentBuilderFactory", exception);
+		}
+
+		return documentBuilderFactory;
+	}
+
+	public static SAXParserFactory newSAXParserFactory() {
+		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+
+		try {
+			saxParserFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			saxParserFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			saxParserFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+		}
+		catch (Exception exception) {
+			LiferayCore.logError("Unable to configure secure SAXParserFactory", exception);
+		}
+
+		return saxParserFactory;
+	}
+
+	public static TransformerFactory newTransformerFactory() {
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+		try {
+			transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+			transformerFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
+		}
+		catch (Exception exception) {
+			LiferayCore.logError("Unable to configure secure TransformerFactory", exception);
+		}
+
+		return transformerFactory;
+	}
+
+}
