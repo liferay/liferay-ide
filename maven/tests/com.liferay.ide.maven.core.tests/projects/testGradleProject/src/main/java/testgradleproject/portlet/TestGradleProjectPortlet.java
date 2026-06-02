676e3ec4ca6699e5a4c0e11225e98c3c34390b2d
Kyle Miho <kyle.miho@liferay.com>
2026-06-02 08:41:47 -0700

    LPD-82051 Unzip testGradleProject to make Gradle wrapper change visible

---
+++ maven/tests/com.liferay.ide.maven.core.tests/projects/testGradleProject/src/main/java/testgradleproject/portlet/TestGradleProjectPortlet.java
@@ -0,0 +1,29 @@
+package testgradleproject.portlet;
+
+import testgradleproject.constants.TestGradleProjectPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author andy
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=testGradleProject Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + TestGradleProjectPortletKeys.TestGradleProject,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class TestGradleProjectPortlet extends MVCPortlet {
+}
